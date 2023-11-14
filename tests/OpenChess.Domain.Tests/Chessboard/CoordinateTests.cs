7d024eb39febbe30db02404195638683fde0f71f
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-14 19:50:09 -0300

    test: add unit test for conversion from integers to algebraic notation

--- tests/OpenChess.Domain.Tests/Chessboard/CoordinateTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/CoordinateTests.cs
@@ -6,6 +6,13 @@ public class CoordinateTests
     [TestMethod]
     public void NewInstance_GivenTwoNumbers_ShouldConvertToAlgebraicNotation()
     {
+        Coordinate coordinate = new(0, 0);
+        Coordinate coordinate2 = new(5, 2);
+        Coordinate coordinate3 = new(7, 7);
 
+
+        Assert.AreEqual("A1", coordinate.ToString());
+        Assert.AreEqual("F3", coordinate2.ToString());
+        Assert.AreEqual("H8", coordinate3.ToString());
     }
 }
