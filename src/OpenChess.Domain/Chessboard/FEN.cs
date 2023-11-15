0a3079808c92dd5fdfb5b8de8f454f8ef9b71f78
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-15 12:49:08 -0300

    feat: add FEN class

---
+++ src/OpenChess.Domain/Chessboard/FEN.cs
@@ -0,0 +1,15 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("OpenChess.Test")]
+namespace OpenChess.Domain
+{
+    internal static class FEN
+    {
+        public static bool IsValid(string position)
+        {
+            return true;
+        }
+
+
+    }
+}
