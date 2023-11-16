c767fe8815aec8eaef28189a5c84c18a22df0f5f
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-16 14:59:43 -0300

    feat: add pawn class

---
+++ src/OpenChess.Domain/Chessboard/Pieces/Pawn.cs
@@ -0,0 +1,48 @@
+
+namespace OpenChess.Domain
+{
+    internal class Pawn : Piece
+    {
+        public Pawn(Color color, Coordinate origin) : base(color, origin)
+        {
+        }
+
+        public bool IsFirstMove
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override char Name => Color == Color.Black ? 'p' : 'P';
+
+        public override List<Direction> Directions => Color == Color.Black ? BlackDirections() : WhiteDirections();
+
+        public override int MoveAmount => 1;
+
+        private List<Direction> BlackDirections()
+        {
+            List<Direction> directions = new()
+            {
+                new Down(),
+                new LowerLeft(),
+                new LowerRight()
+            };
+
+            return directions;
+        }
+
+        private List<Direction> WhiteDirections()
+        {
+            List<Direction> directions = new()
+            {
+                new Up(),
+                new UpperLeft(),
+                new UpperRight()
+            };
+
+            return directions;
+        }
+    }
+}
