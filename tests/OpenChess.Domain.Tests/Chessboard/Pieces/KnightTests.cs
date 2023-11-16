390051367f9d4d6763e1ba0ce276692aa0b9c75e
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-16 17:16:29 -0300

    test: ensure that Directions property returns all L shaped directions for knight

--- tests/OpenChess.Domain.Tests/Chessboard/Pieces/KnightTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Pieces/KnightTests.cs
@@ -29,5 +29,28 @@ namespace OpenChess.Tests
             Assert.IsFalse(knight.IsLongRange);
             Assert.IsFalse(knight2.IsLongRange);
         }
+
+        [TestMethod]
+        public void DirectionsProperty_ShouldReturnAllDirections()
+        {
+            Knight knight = new(Color.White, Coordinate.GetInstance("B1"));
+            Knight knight2 = new(Color.Black, Coordinate.GetInstance("B8"));
+
+            List<Direction> directions = new()
+                {
+                    new Direction(1,2),
+                    new Direction(-1,2),
+                    new Direction(1,-2),
+                    new Direction(-1,-2),
+                    new Direction(2,1),
+                    new Direction(2,-1),
+                    new Direction(-2,-1),
+                    new Direction(-2,1)
+                };
+
+
+            CollectionAssert.AreEquivalent(directions, knight.Directions);
+            CollectionAssert.AreEquivalent(directions, knight2.Directions);
+        }
     }
 }
