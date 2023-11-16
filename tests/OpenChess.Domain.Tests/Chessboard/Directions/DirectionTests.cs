b1a33735a7862301937cf30685ebe2b9a19b0b31
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-16 13:14:06 -0300

    test: ensure that X and Y from direction down is correct

--- tests/OpenChess.Domain.Tests/Chessboard/Directions/DirectionTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Directions/DirectionTests.cs
@@ -19,5 +19,19 @@ namespace OpenChess.Tests
             Up up = new();
             Assert.AreEqual(up.Y, 1);
         }
+
+        [TestMethod]
+        public void Down_X_Returns_0()
+        {
+            Down down = new();
+            Assert.AreEqual(down.X, 0);
+
+        }
+        [TestMethod]
+        public void Down_Y_Returns_negative1()
+        {
+            Down down = new();
+            Assert.AreEqual(down.Y, -1);
+        }
     }
 }
