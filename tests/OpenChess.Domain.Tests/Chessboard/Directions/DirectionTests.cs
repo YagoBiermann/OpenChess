84c150059257a1d4b7161f4636d368e189bd7cc1
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-16 13:36:28 -0300

    test: ensure that same objects are equals

--- tests/OpenChess.Domain.Tests/Chessboard/Directions/DirectionTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Directions/DirectionTests.cs
@@ -113,5 +113,37 @@ namespace OpenChess.Tests
             Assert.AreEqual(lowerRight.Y, -1);
         }
 
+        [TestMethod]
+        public void Equals_SameXandY_ShouldReturnTrue()
+        {
+            Up up = new();
+            Up up2 = new();
+            Direction direction = new(5, -2);
+            Direction direction2 = new(5, -2);
+
+            Assert.IsTrue(up.Equals(up2));
+            Assert.IsTrue(direction.Equals(direction2));
+        }
+
+        [TestMethod]
+        public void Equals_DifferentXandY_ShouldReturnFalse()
+        {
+            Up up = new();
+            Down down = new();
+            Direction direction = new(5, 5);
+            Direction direction2 = new(2, 2);
+
+            Assert.IsFalse(up.Equals(down));
+            Assert.IsFalse(direction.Equals(direction2));
+        }
+
+        [TestMethod]
+        public void Equals_DifferenteObjects_ShouldReturnFalse()
+        {
+            Direction direction = new(1, 1);
+            Coordinate coordinate = Coordinate.GetInstance("A1");
+
+            Assert.IsFalse(direction.Equals(coordinate));
+        }
     }
 }
