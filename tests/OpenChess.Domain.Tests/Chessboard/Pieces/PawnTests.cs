98246e688bed37f5aa693ab29aa6fac212460ac6
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-16 14:50:43 -0300

    test:ensure that black pawn directions are downwards

--- tests/OpenChess.Domain.Tests/Chessboard/Pieces/PawnTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Pieces/PawnTests.cs
@@ -12,5 +12,21 @@ namespace OpenChess.Tests
 
             Assert.AreEqual(pawn.MoveAmount, 1);
         }
+
+        [TestMethod]
+        public void NewInstance_BlackDirections_ShouldBeDownwards()
+        {
+            Pawn pawn = new(Color.Black, Coordinate.GetInstance("A2"));
+
+            Assert.IsTrue(pawn.Directions.Contains(new Down()));
+            Assert.IsTrue(pawn.Directions.Contains(new LowerLeft()));
+            Assert.IsTrue(pawn.Directions.Contains(new LowerRight()));
+
+            Assert.IsFalse(pawn.Directions.Contains(new UpperRight()));
+            Assert.IsFalse(pawn.Directions.Contains(new UpperLeft()));
+            Assert.IsFalse(pawn.Directions.Contains(new Up()));
+            Assert.IsFalse(pawn.Directions.Contains(new Left()));
+            Assert.IsFalse(pawn.Directions.Contains(new Right()));
+        }
     }
 }
