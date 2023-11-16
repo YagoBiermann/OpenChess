677525fe75099ffd16a965f79c273f2b6eed42e2
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-16 16:59:18 -0300

    test: ensure that IsLongRange property returns true for bishop

--- tests/OpenChess.Domain.Tests/Chessboard/Pieces/BishopTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Pieces/BishopTests.cs
@@ -19,5 +19,15 @@ namespace OpenChess.Tests
 
             Assert.AreEqual(bishop.Name, 'B');
         }
+
+        [TestMethod]
+        public void IsLongRangeProperty_ShouldBeTrue()
+        {
+            Bishop bishop = new(Color.White, Coordinate.GetInstance("C1"));
+            Bishop bishop2 = new(Color.Black, Coordinate.GetInstance("C1"));
+
+            Assert.IsTrue(bishop.IsLongRange);
+            Assert.IsTrue(bishop2.IsLongRange);
+        }
     }
 }
