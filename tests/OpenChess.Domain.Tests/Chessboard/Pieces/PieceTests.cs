dff876a8530cafe1dd5c5ff37efad37d649a4c3b
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-16 23:34:16 -0300

    test: ensure that Equals returns false for different objects

--- tests/OpenChess.Domain.Tests/Chessboard/Pieces/PieceTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Pieces/PieceTests.cs
@@ -13,5 +13,32 @@ namespace OpenChess.Tests
 
             Assert.IsTrue(pawn.Equals(pawn2));
         }
+
+        [TestMethod]
+        public void Equals_DifferentPieces_ShouldReturnFalse()
+        {
+            Rook rook = new(Color.Black, Coordinate.GetInstance("B2"));
+            Queen queen = new(Color.White, Coordinate.GetInstance("F4"));
+
+            Assert.IsFalse(rook.Equals(queen));
+        }
+
+        [TestMethod]
+        public void Equals_SamePieceDifferentColors_ShouldReturnFalse()
+        {
+            King king = new(Color.White, Coordinate.GetInstance("F4"));
+            King king2 = new(Color.Black, Coordinate.GetInstance("F4"));
+
+            Assert.IsFalse(king.Equals(king2));
+        }
+
+        [TestMethod]
+        public void Equals_SamePieceDifferentOrigin_ShouldReturnFalse()
+        {
+            Bishop bishop = new(Color.White, Coordinate.GetInstance("A1"));
+            Bishop bishop2 = new(Color.White, Coordinate.GetInstance("A2"));
+
+            Assert.IsFalse(bishop.Equals(bishop2));
+        }
     }
 }
