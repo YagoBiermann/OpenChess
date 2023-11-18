bb10232b206176b1290223c1da5971966519bc76
Yago Biermann <bsfyagobsf@gmail.com>
2023-11-17 22:13:55 -0300

    test: ensure that uppercase char creates white pieces

--- tests/OpenChess.Domain.Tests/Chessboard/Pieces/PieceTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Pieces/PieceTests.cs
@@ -40,5 +40,32 @@ namespace OpenChess.Tests
 
             Assert.IsFalse(bishop.Equals(bishop2));
         }
+
+        [TestMethod]
+        public void Create_UpperChar_ShouldCreateWhitePiece()
+        {
+            Coordinate coordinate = Coordinate.GetInstance("A1");
+
+            Piece king = Piece.Create('K', coordinate);
+            Piece queen = Piece.Create('Q', coordinate);
+            Piece rook = Piece.Create('R', coordinate);
+            Piece bishop = Piece.Create('B', coordinate);
+            Piece knight = Piece.Create('N', coordinate);
+            Piece pawn = Piece.Create('P', coordinate);
+
+            Assert.IsInstanceOfType(king, typeof(King));
+            Assert.IsInstanceOfType(queen, typeof(Queen));
+            Assert.IsInstanceOfType(rook, typeof(Rook));
+            Assert.IsInstanceOfType(bishop, typeof(Bishop));
+            Assert.IsInstanceOfType(knight, typeof(Knight));
+            Assert.IsInstanceOfType(pawn, typeof(Pawn));
+
+            Assert.AreEqual(king.Color, Color.White);
+            Assert.AreEqual(queen.Color, Color.White);
+            Assert.AreEqual(rook.Color, Color.White);
+            Assert.AreEqual(bishop.Color, Color.White);
+            Assert.AreEqual(knight.Color, Color.White);
+            Assert.AreEqual(pawn.Color, Color.White);
+        }
     }
 }
