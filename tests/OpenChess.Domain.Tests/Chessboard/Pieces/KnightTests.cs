e5a1d014cfe987cfb25be473391cca60f3c088b3
YagoBiermann <bsfyagobsf@gmail.com>
2023-11-21 18:47:14 -0300

    test: ensure that knight moves in right direction

--- tests/OpenChess.Domain.Tests/Chessboard/Pieces/KnightTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Pieces/KnightTests.cs
@@ -52,5 +52,33 @@ namespace OpenChess.Tests
             CollectionAssert.AreEquivalent(directions, knight.Directions);
             CollectionAssert.AreEquivalent(directions, knight2.Directions);
         }
+
+        [TestMethod]
+        public void CalculateMoveRange_ShouldReturnAllMoves()
+        {
+            Knight knight = new(Color.White, Coordinate.GetInstance("E4"));
+
+            List<Move> expectedMoves = new()
+            {
+                ExpectedMoves.GetMove(knight.Origin, new Direction(1,2), knight.MoveAmount),
+                ExpectedMoves.GetMove(knight.Origin, new Direction(-1,2), knight.MoveAmount),
+                ExpectedMoves.GetMove(knight.Origin, new Direction(1,-2), knight.MoveAmount),
+                ExpectedMoves.GetMove(knight.Origin, new Direction(-1,-2), knight.MoveAmount),
+                ExpectedMoves.GetMove(knight.Origin, new Direction(2,1), knight.MoveAmount),
+                ExpectedMoves.GetMove(knight.Origin, new Direction(2,-1), knight.MoveAmount),
+                ExpectedMoves.GetMove(knight.Origin, new Direction(-2,-1), knight.MoveAmount),
+                ExpectedMoves.GetMove(knight.Origin, new Direction(-2,1), knight.MoveAmount),
+            };
+
+            List<Move> moves = knight.CalculateMoveRange();
+
+            Assert.AreEqual(moves.Count, expectedMoves.Count);
+            foreach (Move move in moves)
+            {
+                int index = moves.IndexOf(move);
+                CollectionAssert.AreEqual(expectedMoves[index].Coordinates, move.Coordinates);
+                Assert.AreEqual(expectedMoves[index].Direction, move.Direction);
+            }
+        }
     }
 }
