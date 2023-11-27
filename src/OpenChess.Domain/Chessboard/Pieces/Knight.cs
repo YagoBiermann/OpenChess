d670bc1b229f54542bd162b023f0b88960d47852
YagoBiermann <bsfyagobsf@gmail.com>
2023-11-27 17:38:59 -0300

    feat: add specific algorithm to calculate legal moves of knight

--- src/OpenChess.Domain/Chessboard/Pieces/Knight.cs
+++ src/OpenChess.Domain/Chessboard/Pieces/Knight.cs
@@ -10,6 +10,29 @@ namespace OpenChess.Domain
         public override char Name => Color == Color.Black ? 'n' : 'N';
         public override bool IsLongRange => false;
 
+        public override List<Move> CalculateLegalMoves(Chessboard chessboard)
+        {
+            List<Move> legalMoves = new() { };
+            List<Move> moveRange = CalculateMoveRange();
+
+            foreach (Move move in moveRange)
+            {
+                List<Coordinate> currentPosition = move.Coordinates;
+                bool isOutOfChessboard = !currentPosition.Any();
+                if (isOutOfChessboard) { legalMoves.Add(new(move.Direction, currentPosition)); continue; };
+
+                Square currentSquare = chessboard.GetSquare(currentPosition.FirstOrDefault()!);
+                if (!currentSquare.HasPiece) { legalMoves.Add(new(move.Direction, currentPosition)); continue; }
+
+                bool isAllyPieceOrKing = !currentSquare.HasEnemyPiece(Color) || currentSquare.HasTypeOfPiece(typeof(King));
+                if (isAllyPieceOrKing) { legalMoves.Add(new(move.Direction, new())); continue; };
+
+                legalMoves.Add(new(move.Direction, currentPosition));
+            }
+
+            return legalMoves;
+        }
+
         public override List<Direction> Directions
         {
             get
