d86d70c9ca0e156ce715331d71142992c39cf781
YagoBiermann <bsfyagobsf@gmail.com>
2023-11-29 14:07:55 -0300

    feat: determine whether a player is in check or not

---
+++ src/OpenChess.Domain/Rules/Check.cs
@@ -0,0 +1,23 @@
+namespace OpenChess.Domain
+{
+    internal static class Check
+    {
+        public static int CalculateCheckAmount(Color player, Chessboard chessboard)
+        {
+            Color enemyPlayer = player == Color.Black ? Color.White : Color.Black;
+            List<Coordinate> piecePositions = chessboard.GetAllPiecesFromPlayer(enemyPlayer);
+            int checkAmount = 0;
+            foreach (Coordinate origin in piecePositions)
+            {
+                if (chessboard.GetSquare(origin).Piece!.IsHittingTheEnemyKing(chessboard)) { checkAmount++; };
+            }
+
+            return checkAmount;
+        }
+        
+        public static bool IsInCheck(Color player, Chessboard chessboard)
+        {
+            return CalculateCheckAmount(player, chessboard) > 0;
+        }
+    }
+}
