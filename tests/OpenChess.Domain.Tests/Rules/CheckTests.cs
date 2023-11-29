d6b48fce40bbfd6167fa2885250e54c206903494
YagoBiermann <bsfyagobsf@gmail.com>
2023-11-29 14:14:47 -0300

    test: ensure that IsInCheck returns true when a player is in check

---
+++ tests/OpenChess.Domain.Tests/Rules/CheckTests.cs
@@ -0,0 +1,25 @@
+using OpenChess.Domain;
+
+namespace OpenChess.Tests
+{
+    [TestClass]
+    public class CheckTests
+    {
+        [DataRow('b', "r7/3R2k1/4P3/4K3/8/8/8/8 w - - 0 1")]
+        [DataRow('b', "rn1qkb1r/ppp2pp1/5n1p/1B1p2B1/3P2b1/4P1P1/PP3P1P/RN1QK1NR b KQkq - 0 1")]
+        [DataRow('b', "rn1qkb1r/pp1n2p1/2p2p1p/1B1p2BQ/3P4/4P1P1/PP3P1P/RN2K1NR b KQkq - 0 1")]
+        [DataRow('b', "3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1")]
+        [DataRow('w', "3bk3/8/4Pp2/4K3/8/8/4B3/8 w - - 0 1")]
+        [DataRow('w', "r5K1/3R4/4Pk2/8/8/8/8/8 w - - 0 1")]
+        [DataRow('w', "rnb1kbnr/pppp1ppp/8/8/2B1Pp1q/8/PPPP2PP/RNBQK1NR b KQkq - 0 1")]
+        [DataRow('w', "rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1")]
+        [TestMethod]
+        public void IsInCheck_PlayerInCheck_ShouldReturnTrue(char color, string fen)
+        {
+            Chessboard chessboard = new(fen);
+            Color player = color == 'w' ? Color.White : Color.Black;
+
+            Assert.IsTrue(Check.IsInCheck(player, chessboard));
+        }
+    }
+}
