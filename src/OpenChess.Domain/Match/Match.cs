3dbc578a8c06a703b635cf8f5c76aafc083da50d
YagoBiermann <bsfyagobsf@gmail.com>
2023-11-29 22:05:28 -0300

    feat: add match properties

--- src/OpenChess.Domain/Match/Match.cs
+++ src/OpenChess.Domain/Match/Match.cs
@@ -2,6 +2,12 @@ namespace OpenChess.Domain
 {
     internal class Match
     {
-
+        Guid Id { get; }
+        private List<Player> _players = new(2);
+        private Chessboard _chessboard { get; }
+        private Player _currentPlayer { get; set; }
+        private Stack<string> _pgn { get; }
+        private MatchStatus _status { get; set; }
+        private Player? _winner { get; set; }
     }
 }
