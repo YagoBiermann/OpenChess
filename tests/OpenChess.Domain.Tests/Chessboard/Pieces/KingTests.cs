847f22640433194ca3173e01aaf864ed97aa31e2
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-03 18:29:44 -0300

    refactor: remove unused parameter

--- tests/OpenChess.Domain.Tests/Chessboard/Pieces/KingTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Pieces/KingTests.cs
@@ -91,7 +91,7 @@ namespace OpenChess.Tests
             Chessboard chessboard = new("8/4R3/5KB1/4p3/8/8/3r1P2/2q1k3 b - - 0 1");
             King king = (King)chessboard.GetSquare(Coordinate.GetInstance("E1")).Piece!;
 
-            List<Coordinate> moves = king.CalculateLegalMoves(chessboard).Find(m => m.Direction.Equals(new UpperRight())).Coordinates;
+            List<Coordinate> moves = king.CalculateLegalMoves().Find(m => m.Direction.Equals(new UpperRight())).Coordinates;
             List<Coordinate> expectedMove = new() { Coordinate.GetInstance("F2") };
 
             CollectionAssert.AreEqual(expectedMove, moves);
@@ -103,7 +103,7 @@ namespace OpenChess.Tests
             Chessboard chessboard = new("8/4R3/5KB1/4p3/8/8/3r1P2/2q1k3 b - - 0 1");
             King king = (King)chessboard.GetSquare(Coordinate.GetInstance("E1")).Piece!;
 
-            List<Coordinate> moves = king.CalculateLegalMoves(chessboard).Find(m => m.Direction.Equals(new UpperLeft())).Coordinates;
+            List<Coordinate> moves = king.CalculateLegalMoves().Find(m => m.Direction.Equals(new UpperLeft())).Coordinates;
 
             Assert.IsFalse(moves.Any());
         }
@@ -114,7 +114,7 @@ namespace OpenChess.Tests
             Chessboard chessboard = new("8/8/8/8/3k4/3K4/8/8 w - - 0 1");
             King king = (King)chessboard.GetSquare(Coordinate.GetInstance("D3")).Piece!;
 
-            List<MovePositions> moves = king.CalculateLegalMoves(chessboard);
+            List<MovePositions> moves = king.CalculateLegalMoves();
             List<Coordinate> up = moves.Find(m => m.Direction.Equals(new Up())).Coordinates;
 
             Assert.IsFalse(up.Any());
@@ -126,7 +126,7 @@ namespace OpenChess.Tests
             Chessboard chessboard = new("8/4R3/5KB1/4p3/8/8/3r1P2/2q1k3 b - - 0 1");
             King king = (King)chessboard.GetSquare(Coordinate.GetInstance("E1")).Piece!;
 
-            List<MovePositions> moves = king.CalculateLegalMoves(chessboard);
+            List<MovePositions> moves = king.CalculateLegalMoves();
             List<Coordinate> up = moves.Find(m => m.Direction.Equals(new Up())).Coordinates;
             List<Coordinate> left = moves.Find(m => m.Direction.Equals(new Left())).Coordinates;
             List<Coordinate> right = moves.Find(m => m.Direction.Equals(new Right())).Coordinates;
