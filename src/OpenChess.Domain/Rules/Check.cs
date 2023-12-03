44129fbc8ab1af808145fac4ef4c4980d77b6256
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-03 18:27:15 -0300

    refactor: remove unused parameter

--- src/OpenChess.Domain/Rules/Check.cs
+++ src/OpenChess.Domain/Rules/Check.cs
@@ -9,12 +9,12 @@ namespace OpenChess.Domain
             int checkAmount = 0;
             foreach (Coordinate origin in piecePositions)
             {
-                if (chessboard.GetSquare(origin).Piece!.IsHittingTheEnemyKing(chessboard)) { checkAmount++; };
+                if (chessboard.GetSquare(origin).Piece!.IsHittingTheEnemyKing()) { checkAmount++; };
             }
 
             return checkAmount;
         }
-        
+
         public static bool IsInCheck(Color player, Chessboard chessboard)
         {
             return CalculateCheckAmount(player, chessboard) > 0;
