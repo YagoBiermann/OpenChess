a8e2ac4410b742f1dd8d514ec2d6c1e3a25c7d05
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-03 23:23:32 -0300

    refactor: remove code duplication

--- tests/OpenChess.Domain.Tests/Chessboard/FENTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/FENTests.cs
@@ -5,20 +5,15 @@ namespace OpenChess.Tests
     [TestClass]
     public class FENTests
     {
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")]
+        [DataRow("4r1k1/3b1ppp/rq1p1b2/1p2p3/1P2P3/4QN1P/5PP1/RN2K2R w KQ - 0 19")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 w - - 11 63")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 w - F3 11 63")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 w - F6 11 63")]
         [TestMethod]
-        public void IsValid_GivenValidFEN_ShouldReturnTrue()
+        public void IsValid_GivenValidFEN_ShouldReturnTrue(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-            string position2 = "4r1k1/3b1ppp/rq1p1b2/1p2p3/1P2P3/4QN1P/5PP1/RN2K2R w KQ - 0 19";
-            string position3 = "8/6b1/4k1P1/1q6/q7/K7/8/8 w - - 11 63";
-            string position4 = "8/6b1/4k1P1/1q6/q7/K7/8/8 w - F3 11 63";
-            string position5 = "8/6b1/4k1P1/1q6/q7/K7/8/8 w - F6 11 63";
-
             Assert.IsTrue(FEN.IsValid(position));
-            Assert.IsTrue(FEN.IsValid(position2));
-            Assert.IsTrue(FEN.IsValid(position3));
-            Assert.IsTrue(FEN.IsValid(position4));
-            Assert.IsTrue(FEN.IsValid(position5));
         }
 
         [TestMethod]
@@ -27,42 +22,32 @@ namespace OpenChess.Tests
             Assert.AreEqual("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", FEN.InitialPosition);
         }
 
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 InvalidField")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR Test w KQkq - 0 1")]
         [TestMethod]
-        public void IsValid_MoreThanSixFields_ShouldReturnFalse()
+        public void IsValid_MoreThanSixFields_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 InvalidField";
-            string position2 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 1";
-            string position3 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR Test w KQkq - 0 1";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
         }
 
+        [DataRow("w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR KQkq - 0 1")]
         [TestMethod]
-        public void IsValid_LessThanSixFields_ShouldReturnFalse()
+        public void IsValid_LessThanSixFields_ShouldReturnFalse(string position)
         {
-            string position = "w KQkq - 0 1";
-            string position2 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - 0 1";
-            string position3 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR KQkq - 0 1";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
         }
 
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/PPPPPPPP w KQkq - 0 1")]
+        [DataRow("rnbqkbnr")]
+        [DataRow("")]
         [TestMethod]
-        public void IsValid_LessThanEightColumns_ShouldReturnFalse()
+        public void IsValid_LessThanEightColumns_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP w KQkq - 0 1";
-            string position2 = "rnbqkbnr/pppppppp/8/8/8/PPPPPPPP w KQkq - 0 1";
-            string position3 = "rnbqkbnr";
-            string position4 = "";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
-            Assert.IsFalse(FEN.IsValid(position4));
         }
 
         [TestMethod]
@@ -73,110 +58,78 @@ namespace OpenChess.Tests
             Assert.IsFalse(FEN.IsValid(position));
         }
 
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR// w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP//RNBQKBNR w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8//8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")]
+        [DataRow("//rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")]
         [TestMethod]
-        public void IsValid_DuplicatedSlashes_ShouldReturnFalse()
+        public void IsValid_DuplicatedSlashes_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR// w KQkq - 0 1";
-            string position2 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP//RNBQKBNR w KQkq - 0 1";
-            string position3 = "rnbqkbnr/pppppppp/8/8/8//8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-            string position4 = "//rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
-            Assert.IsFalse(FEN.IsValid(position4));
         }
 
+        [DataRow("rnbqkbnr/ppppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/9/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQ2BNR w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/P/RNBQKBNR w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/P8/RNBQKBNR w KQkq - 0 1")]
         [TestMethod]
-        public void IsValid_InvalidRow_ShouldReturnFalse()
+        public void IsValid_InvalidRow_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/ppppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-            string position2 = "rnbqkbnr/pppppppp/9/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-            string position3 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQ2BNR w KQkq - 0 1";
-            string position4 = "rnbqkbnr/pppppppp/8/8/8/8/P/RNBQKBNR w KQkq - 0 1";
-            string position5 = "rnbqkbnr/pppppppp/8/8/8/8/P8/RNBQKBNR w KQkq - 0 1";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
-            Assert.IsFalse(FEN.IsValid(position4));
-            Assert.IsFalse(FEN.IsValid(position5));
         }
 
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR x KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR 0 KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR B KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR W KQkq - 0 1")]
         [TestMethod]
-        public void IsValid_InvalidActiveColor_ShouldReturnFalse()
+        public void IsValid_InvalidActiveColor_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR x KQkq - 0 1";
-            string position2 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR 0 KQkq - 0 1";
-            string position3 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR B KQkq - 0 1";
-            string position4 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR W KQkq - 0 1";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
-            Assert.IsFalse(FEN.IsValid(position4));
         }
 
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq 22 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq 1 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq FW 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq E4 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq E5 0 1")]
         [TestMethod]
-        public void IsValid_InvalidEnPassant_ShouldReturnFalse()
+        public void IsValid_InvalidEnPassant_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq 22 0 1";
-            string position2 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq 1 0 1";
-            string position3 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq FW 0 1";
-            string position4 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq E4 0 1";
-            string position5 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq E5 0 1";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
-            Assert.IsFalse(FEN.IsValid(position4));
-            Assert.IsFalse(FEN.IsValid(position5));
         }
 
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQ-q - 0 1")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 w PWXQ - 11 63")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 b -- - 11 63")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 b 1234 - 11 63")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w kqKQ - 0 1")]
         [TestMethod]
-        public void IsValidString_InvalidCastling_ShouldReturnFalse()
+        public void IsValidString_InvalidCastling_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQ-q - 0 1";
-            string position2 = "8/6b1/4k1P1/1q6/q7/K7/8/8 w PWXQ - 11 63";
-            string position3 = "8/6b1/4k1P1/1q6/q7/K7/8/8 b -- - 11 63";
-            string position4 = "8/6b1/4k1P1/1q6/q7/K7/8/8 b 1234 - 11 63";
-            string position5 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w kqKQ - 0 1";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
-            Assert.IsFalse(FEN.IsValid(position4));
-            Assert.IsFalse(FEN.IsValid(position5));
         }
 
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQ-q - 0 1")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 b - - TEST 63")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 b - - 101 63")]
+        [DataRow("8/6b1/4k1P1/1q6/q7/K7/8/8 b - - -1 63")]
         [TestMethod]
-        public void IsValidString_InvalidHalfMove_ShouldReturnFalse()
+        public void IsValidString_InvalidHalfMove_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQ-q - 0 1";
-            string position2 = "8/6b1/4k1P1/1q6/q7/K7/8/8 b - - TEST 63";
-            string position3 = "8/6b1/4k1P1/1q6/q7/K7/8/8 b - - 101 63";
-            string position4 = "8/6b1/4k1P1/1q6/q7/K7/8/8 b - - -1 63";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
-            Assert.IsFalse(FEN.IsValid(position4));
         }
 
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 -1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1999")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1201")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 00")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 FF")]
         [TestMethod]
-        public void IsValidString_InvalidFullMove_ShouldReturnFalse()
+        public void IsValidString_InvalidFullMove_ShouldReturnFalse(string position)
         {
-            string position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 -1";
-            string position2 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1999";
-            string position3 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1201";
-            string position4 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 00";
-            string position5 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 FF";
-
             Assert.IsFalse(FEN.IsValid(position));
-            Assert.IsFalse(FEN.IsValid(position2));
-            Assert.IsFalse(FEN.IsValid(position3));
-            Assert.IsFalse(FEN.IsValid(position4));
-            Assert.IsFalse(FEN.IsValid(position5));
         }
     }
 }
