78207dc9d497835af77b20c8840a5525281a2dc6
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-04 00:51:11 -0300

    feat: add method overload

--- src/OpenChess.Domain/Chessboard/Chessboard.cs
+++ src/OpenChess.Domain/Chessboard/Chessboard.cs
@@ -21,6 +21,12 @@ namespace OpenChess.Domain
             FullMove = ConvertMoveAmount(fenPosition.FullMove);
         }
 
+        public Square GetSquare(string coordinate)
+        {
+            Coordinate origin = Coordinate.GetInstance(coordinate);
+            return GetSquare(origin);
+        }
+
         public Square GetSquare(Coordinate coordinate)
         {
             return _board[coordinate.RowToInt][coordinate.ColumnToInt];
