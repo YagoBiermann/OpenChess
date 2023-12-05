c91491c1b2f28303e0ec413d7ef7f7ff2bcf200e
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-04 23:00:37 -0300

    refactor: add private set to chessboard

--- src/OpenChess.Domain/Match/Match.cs
+++ src/OpenChess.Domain/Match/Match.cs
@@ -4,7 +4,7 @@ namespace OpenChess.Domain
     {
         public Guid Id { get; }
         private List<Player> _players = new(2);
-        private Chessboard _chessboard { get; }
+        private Chessboard _chessboard { get; set; }
         private Stack<string> _pgn = new();
         private MatchStatus _status { get; set; }
         private Player? _winner { get; set; }
