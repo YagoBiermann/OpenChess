f7bcda4497f9b5e3210cbf2ae0559c1d257648c0
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-07 02:18:48 -0300

    test: ensure that checkmate sign is appended correctly to pawn move

--- tests/OpenChess.Domain.Tests/Match/PGNBuilderTests.cs
+++ tests/OpenChess.Domain.Tests/Match/PGNBuilderTests.cs
@@ -29,5 +29,17 @@ namespace OpenChess.Tests
 
             Assert.AreEqual("1. e5+", move);
         }
+
+        [TestMethod]
+        public void PawnTextMoveBuilder_Build_MoveWithCheckmate_ShouldAddCheckmateSign()
+        {
+            Coordinate origin = Coordinate.GetInstance("E4");
+            Coordinate destination = Coordinate.GetInstance("E5");
+            PawnTextMoveBuilder builder = new(1, origin, destination);
+
+            string move = builder.Build().AppendCheckMateSign().Result;
+
+            Assert.AreEqual("1. e5#", move);
+        }
     }
 }
