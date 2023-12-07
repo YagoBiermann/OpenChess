7005f4f3be1300c2d6b258805e46affef16c0fb6
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-07 02:16:19 -0300

    test: ensure that pawn text move is built correctly

---
+++ tests/OpenChess.Domain.Tests/Match/PGNBuilderTests.cs
@@ -0,0 +1,21 @@
+using OpenChess.Domain;
+
+namespace OpenChess.Tests
+{
+    [TestClass]
+    public class PGNBuilderTests
+    {
+
+        [TestMethod]
+        public void PawnTextMoveBuilder_Build_ShouldCreateDefaultMove()
+        {
+            Coordinate origin = Coordinate.GetInstance("E4");
+            Coordinate destination = Coordinate.GetInstance("E5");
+            PawnTextMoveBuilder builder = new(1, origin, destination);
+
+            string move = builder.Build().Result;
+
+            Assert.AreEqual("1. e5", move);
+        }
+    }
+}
