21480756f30154c0b0260ed4e2d99b301db9af72
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-08 19:27:08 -0300

    feat: implement class to handle en passant moves

---
+++ src/OpenChess.Domain/Rules/EnPassant.cs
@@ -0,0 +1,39 @@
+namespace OpenChess.Domain
+{
+    internal static class EnPassant
+    {
+        public static bool IsEnPassantMove(Coordinate origin, Coordinate destination, IReadOnlyChessboard chessboard)
+        {
+            IReadOnlyPiece? piece = chessboard.GetReadOnlySquare(origin).ReadOnlyPiece;
+
+            if (piece is not Pawn) return false;
+            if (destination == chessboard.EnPassant) return true;
+            return false;
+        }
+
+        public static IReadOnlyPiece? Handle(Coordinate origin, Coordinate destination, Chessboard chessboard)
+        {
+            IReadOnlyPiece? piece = chessboard.GetReadOnlySquare(origin).ReadOnlyPiece;
+            if (piece is not Pawn pawn) return null;
+            if (IsEnPassantMove(origin, destination, chessboard) && pawn.CanCaptureByEnPassant)
+            {
+                chessboard.ChangePiecePosition(origin, destination);
+
+                Coordinate vulnerablePawnPosition = chessboard.GetVulnerablePawn!.Origin;
+                chessboard.RemovePiece(vulnerablePawnPosition);
+
+            }
+
+            return piece;
+        }
+
+        public static void UpdateState(IReadOnlyPiece? piece, Chessboard chessboard)
+        {
+            chessboard.EnPassant = null;
+            if (piece is not Pawn pawn) return;
+            if (!pawn.IsVulnerableToEnPassant) return;
+
+            chessboard.EnPassant = pawn.GetEnPassantPosition;
+        }
+    }
+}
