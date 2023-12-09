7c8c54d46b2f4ccda130caffcb983506302e4bd5
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-09 18:10:03 -0300

    feat: add method to check whether is promoting move or not

--- src/OpenChess.Domain/Chessboard/Promotion.cs
+++ src/OpenChess.Domain/Chessboard/Promotion.cs
@@ -10,5 +10,16 @@ namespace OpenChess.Domain
         {
             _chessboard = chessboard;
         }
+
+        public bool IsPromoting(Coordinate origin, Coordinate destination)
+        {
+            IReadOnlySquare square = _chessboard.GetReadOnlySquare(origin);
+            if (square.ReadOnlyPiece is not Pawn pawn) return false;
+
+            bool isWhitePromoting = pawn.ForwardDirection is Up && destination.Row == '8';
+            bool isBlackPromoting = pawn.ForwardDirection is Down && destination.Row == '1';
+
+            return isWhitePromoting ^ isBlackPromoting;
+        }
     }
 }
