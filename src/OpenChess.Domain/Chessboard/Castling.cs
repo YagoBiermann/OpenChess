0cbfe2e0e90af4fcbba59eb68cd9cf419a0c3b58
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-10 14:33:41 -0300

    feat: detect castling moves

--- src/OpenChess.Domain/Chessboard/Castling.cs
+++ src/OpenChess.Domain/Chessboard/Castling.cs
@@ -7,7 +7,6 @@ namespace OpenChess.Domain
         public bool HasBlackKingSide { get; init; }
         public bool HasBlackQueenSide { get; init; }
 
-
         public Castling()
         {
             HasWhiteKingSide = true;
@@ -42,6 +41,46 @@ namespace OpenChess.Domain
             if (!HasWhiteKingSide && !HasWhiteQueenSide && !HasBlackKingSide && !HasBlackQueenSide) castlingAvailability += "-";
 
             return castlingAvailability;
+        }
+
+        public static bool IsCastling(Coordinate origin, Coordinate destination, IReadOnlyChessboard chessboard)
+        {
+            bool isNotKingPosition = origin.Column != 'E' || !(origin.Row == '1' || origin.Row == '8');
+            if (isNotKingPosition) return false;
+            IReadOnlyPiece? piece = chessboard.GetReadOnlySquare(origin).ReadOnlyPiece;
+            if (piece is not King) return false;
+            if (origin.Row == '1' && !WhiteCastlingPositions.Contains(destination)) return false;
+            if (origin.Row == '8' && !BlackCastlingPositions.Contains(destination)) return false;
+
+            return true;
+        }
+
+        private static List<Coordinate> WhiteCastlingPositions
+        {
+            get
+            {
+                List<Coordinate> positions = new()
+                {
+                    Coordinate.GetInstance("G1"),
+                    Coordinate.GetInstance("C1"),
+                };
+
+                return positions;
+            }
+        }
+
+        private static List<Coordinate> BlackCastlingPositions
+        {
+            get
+            {
+                List<Coordinate> positions = new()
+                {
+                    Coordinate.GetInstance("G8"),
+                    Coordinate.GetInstance("C8"),
+                };
+
+                return positions;
+            }
         }
     }
 }
