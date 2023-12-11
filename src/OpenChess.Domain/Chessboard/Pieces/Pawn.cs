760cab91e8b9700dbef25b3fa569e55dcb4ba6bc
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-11 18:35:29 -0300

    refactor: move algorithm to calculate pawn legal moves into its own class

--- src/OpenChess.Domain/Chessboard/Pieces/Pawn.cs
+++ src/OpenChess.Domain/Chessboard/Pieces/Pawn.cs
@@ -56,61 +56,6 @@ namespace OpenChess.Domain
             return moves;
         }
 
-        public override List<MoveDirections> CalculateLegalMoves()
-        {
-            List<MoveDirections> legalMoves = new();
-            List<MoveDirections> moveRange = CalculateMoveRange();
-
-            foreach (MoveDirections move in moveRange)
-            {
-                Direction currentDirection = move.Direction;
-                List<Coordinate> currentCoordinates = move.Coordinates;
-
-                if (currentDirection.Equals(ForwardDirection))
-                {
-                    List<Coordinate> forwardMoves = CalculateForwardMoves(Chessboard, currentCoordinates);
-                    legalMoves.Add(new(currentDirection, forwardMoves));
-                    continue;
-                };
-
-                List<Coordinate> emptyList = new();
-                MoveDirections emptyPosition = new(currentDirection, emptyList);
-                MoveDirections sameCurrentPosition = new(currentDirection, currentCoordinates);
-
-                Coordinate? diagonal = currentCoordinates.FirstOrDefault();
-                bool diagonalIsOutOfChessboard = diagonal is null;
-                if (diagonalIsOutOfChessboard) { legalMoves.Add(emptyPosition); continue; };
-
-                IReadOnlySquare square = Chessboard.GetReadOnlySquare(diagonal!);
-                bool isEnPassant = diagonal!.Equals(Chessboard.EnPassant.Position);
-                if (isEnPassant) { legalMoves.Add(sameCurrentPosition); continue; };
-                if (!square.HasPiece) { legalMoves.Add(emptyPosition); continue; }
-                bool hasAllyPiece = !square.HasEnemyPiece(Color);
-                bool hasKing = square.HasTypeOfPiece(typeof(King));
-                if (hasAllyPiece || hasKing) { legalMoves.Add(emptyPosition); continue; }
-
-                legalMoves.Add(sameCurrentPosition);
-            }
-
-            return legalMoves;
-        }
-
-        private List<Coordinate> CalculateForwardMoves(IReadOnlyChessboard chessboard, List<Coordinate> forwardCoordinates)
-        {
-            List<Coordinate> piecesPosition = chessboard.GetPiecesPosition(forwardCoordinates);
-
-            List<CoordinateDistances> distances = CoordinateDistances.CalculateDistance(Origin, piecesPosition);
-            bool noPiecesForward = !distances.Any();
-            if (noPiecesForward) return forwardCoordinates;
-            CoordinateDistances nearestPiece = CoordinateDistances.CalculateNearestDistance(distances);
-
-            List<Coordinate> forwardMoves = Coordinate.CalculateSequence(Origin, ForwardDirection, nearestPiece.DistanceBetween);
-            int lastPosition = forwardMoves.Count - 1;
-            forwardMoves.RemoveAt(lastPosition);
-
-            return forwardMoves;
-        }
-
         private static List<Direction> BlackDirections()
         {
             List<Direction> directions = new()
