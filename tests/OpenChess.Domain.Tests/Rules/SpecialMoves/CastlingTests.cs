2e6fca83af7cedd9229f89e3e9804ab2e0acf1fa
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-13 01:10:17 -0300

    refactor: remove unnecessary test

--- tests/OpenChess.Domain.Tests/Rules/SpecialMoves/CastlingTests.cs
+++ tests/OpenChess.Domain.Tests/Rules/SpecialMoves/CastlingTests.cs
@@ -49,42 +49,5 @@ namespace OpenChess.Tests
             Assert.AreEqual("Qq", castling3.ToString());
             Assert.AreEqual("Kk", castling4.ToString());
         }
-
-        [DataRow("E1", "G1")]
-        [DataRow("E1", "C1")]
-        [DataRow("E8", "G8")]
-        [DataRow("E8", "C8")]
-        [TestMethod]
-        public void IsCastling_ShouldReturnTrue(string position1, string position2)
-        {
-            Chessboard chessboard = new("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1");
-            Coordinate origin = Coordinate.GetInstance(position1);
-            Coordinate destination = Coordinate.GetInstance(position2);
-            Castling castling = new(chessboard);
-            Assert.IsTrue(castling.IsCastling(origin, destination));
-        }
-
-        [DataRow("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1", "E1", "D1")]
-        [DataRow("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1", "E1", "F1")]
-        [DataRow("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1", "E1", "C8")]
-        [DataRow("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1", "F1", "C1")]
-        [DataRow("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1", "E8", "F8")]
-        [DataRow("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1", "E8", "D8")]
-        [DataRow("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1", "E8", "C1")]
-        [DataRow("r3k2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3K2R w KQkq - 0 1", "F8", "E8")]
-        [DataRow("r3Q2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3Q2R w - - 0 1", "E1", "G1")]
-        [DataRow("r3Q2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3Q2R w - - 0 1", "E1", "C1")]
-        [DataRow("r3Q2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3Q2R w - - 0 1", "E8", "C8")]
-        [DataRow("r3Q2r/pp3ppp/2p5/8/8/3P4/PPP2PPP/R3Q2R w - - 0 1", "E8", "G8")]
-        [TestMethod]
-        public void IsCastling_ShouldReturnFalse(string fen, string position1, string position2)
-        {
-            Chessboard chessboard = new(fen);
-            Coordinate origin = Coordinate.GetInstance(position1);
-            Coordinate destination = Coordinate.GetInstance(position2);
-            Castling castling = new(chessboard);
-
-            Assert.IsFalse(castling.IsCastling(origin, destination));
-        }
     }
 }
