6f11f93ea8b32193a90aa23bb29a34e79fbb176a
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-13 01:09:59 -0300

    refactor: improve en passant tests

--- tests/OpenChess.Domain.Tests/Rules/SpecialMoves/EnPassantTests.cs
+++ tests/OpenChess.Domain.Tests/Rules/SpecialMoves/EnPassantTests.cs
@@ -5,80 +5,45 @@ namespace OpenChess.Tests
     [TestClass]
     public class EnPassantTests
     {
-
-        [DataRow("E4", "E3")]
-        [DataRow("E5", "E6")]
+        [DataRow("D4", "E3", "rnbqkbnr/pppp1ppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR w KQkq E3 0 1", 'w', 'b')]
+        [DataRow("D5", "E6", "rnbqkbnr/pppp1ppp/8/3Pp3/8/8/PPP1PPPP/RNBQKBNR w KQkq E6 0 1", 'b', 'w')]
         [TestMethod]
-        public void GetEnPassantPosition_ShouldReturnPositionBehind(string origin, string expected)
-        {
-            Coordinate position = Coordinate.GetInstance(origin);
-            Coordinate expectedPosition = Coordinate.GetInstance(expected);
-            Chessboard chessboard = new("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1");
-            Pawn pawn = (Pawn)chessboard.GetReadOnlySquare(position).ReadOnlyPiece!;
-
-            Assert.AreEqual(expectedPosition, chessboard.EnPassant.GetEnPassantPosition(pawn));
-        }
-
-        [DataRow("E2")]
-        [DataRow("E7")]
-        [TestMethod]
-        public void GetEnPassantPosition_PawnNotVulnerable_ShouldReturnNull(string origin)
-        {
-            Coordinate position = Coordinate.GetInstance(origin);
-            Chessboard chessboard = new(FenInfo.InitialPosition);
-            Pawn pawn = (Pawn)chessboard.GetReadOnlySquare(position).ReadOnlyPiece!;
-
-            Assert.IsNull(chessboard.EnPassant.GetEnPassantPosition(pawn));
-        }
-
-        [DataRow("E4")]
-        [DataRow("E5")]
-        [TestMethod]
-        public void IsVulnerableToEnPassant_PawnVulnerable_ShouldReturnTrue(string origin)
-        {
-            Coordinate position = Coordinate.GetInstance(origin);
-            Chessboard chessboard = new("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1");
-            Pawn pawn = (Pawn)chessboard.GetReadOnlySquare(position).ReadOnlyPiece!;
-
-            Assert.IsTrue(chessboard.EnPassant.IsVulnerableToEnPassant(pawn));
-        }
-
-        [DataRow("E2")]
-        [DataRow("E7")]
-        [TestMethod]
-        public void IsVulnerableToEnPassant_PawnNotVulnerable_ShouldReturnFalse(string origin)
-        {
-            Coordinate position = Coordinate.GetInstance(origin);
-            Chessboard chessboard = new(FenInfo.InitialPosition);
-            Pawn pawn = (Pawn)chessboard.GetReadOnlySquare(position).ReadOnlyPiece!;
-
-            Assert.IsFalse(chessboard.EnPassant.IsVulnerableToEnPassant(pawn));
-        }
-
-        [DataRow("D4", "E4", "rnbqkbnr/pppp1ppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")]
-        [DataRow("D5", "E5", "rnbqkbnr/pppp1ppp/8/3Pp3/8/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1")]
-        [TestMethod]
-        public void CanCaptureByEnPassant_WithinTheRange_ShouldReturnTrue(string origin, string enPassant, string fen)
+        public void MovePiece_PawnVulnerable_ShouldBeCapturedByEnPassant(string position1, string position2, string fen, char color1, char color2)
         {
             Chessboard chessboard = new(fen);
-            IReadOnlyPiece? vulnerablePawn = chessboard.GetReadOnlySquare(Coordinate.GetInstance(enPassant)).ReadOnlyPiece;
-            chessboard.EnPassant.SetVulnerablePawn(vulnerablePawn);
-            Pawn pawn = (Pawn)chessboard.GetReadOnlySquare(Coordinate.GetInstance(origin)).ReadOnlyPiece!;
+            var origin = Coordinate.GetInstance(position1);
+            var destination = Coordinate.GetInstance(position2);
 
-            Assert.IsTrue(chessboard.EnPassant.CanCaptureByEnPassant(pawn));
+            IReadOnlyPiece? pieceCaptured = chessboard.MovePiece(origin, destination);
+
+            Assert.IsInstanceOfType(pieceCaptured, typeof(Pawn));
+            Assert.AreEqual(pieceCaptured.Color, Utils.ColorFromChar(color1));
+            Assert.IsTrue(chessboard.GetReadOnlySquare(destination).HasPiece);
+            Assert.AreEqual(chessboard.GetReadOnlySquare(destination).ReadOnlyPiece!.Color, Utils.ColorFromChar(color2));
         }
 
-        [DataRow("D7", "E4", "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")]
-        [DataRow("D2", "E5", "rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")]
+        [DataRow("D4", "E3", "rnbqkbnr/pppp1ppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")]
+        [DataRow("D5", "E6", "rnbqkbnr/pppp1ppp/8/3Pp3/8/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1")]
         [TestMethod]
-        public void CanCaptureByEnPassant_OutOfTheRange_ShouldReturnFalse(string origin, string enPassant, string fen)
+        public void MovePiece_PawnNotVulnerable_ShouldNotBeCaptured(string position1, string position2, string fen)
         {
             Chessboard chessboard = new(fen);
-            IReadOnlyPiece? vulnerablePawn = chessboard.GetReadOnlySquare(Coordinate.GetInstance(enPassant)).ReadOnlyPiece;
-            chessboard.EnPassant.SetVulnerablePawn(vulnerablePawn);
-            Pawn pawn = (Pawn)chessboard.GetReadOnlySquare(Coordinate.GetInstance(origin)).ReadOnlyPiece!;
+            var origin = Coordinate.GetInstance(position1);
+            var destination = Coordinate.GetInstance(position2);
 
-            Assert.IsFalse(chessboard.EnPassant.CanCaptureByEnPassant(pawn));
+            Assert.ThrowsException<ChessboardException>(() => chessboard.MovePiece(origin, destination));
+        }
+
+        [DataRow("D7", "E4", "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq E3 0 1")]
+        [DataRow("D2", "E5", "rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq E6 0 1")]
+        [TestMethod]
+        public void MovePiece_PawnOutOfRange_ShouldNotBeAbleToCapture(string position1, string position2, string fen)
+        {
+            Chessboard chessboard = new(fen);
+            var origin = Coordinate.GetInstance(position1);
+            var destination = Coordinate.GetInstance(position2);
+
+            Assert.ThrowsException<ChessboardException>(() => chessboard.MovePiece(origin, destination));
         }
     }
 }
