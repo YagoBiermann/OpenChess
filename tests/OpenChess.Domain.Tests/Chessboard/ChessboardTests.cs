56db86df4ee3db4072bf357374b2dd67c365e755
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-13 01:07:51 -0300

    refactor: move tests related to pawn promotion into its own class

--- tests/OpenChess.Domain.Tests/Chessboard/ChessboardTests.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/ChessboardTests.cs
@@ -173,7 +173,6 @@ namespace OpenChess.Tests
             chessboard.SwitchTurns();
             Assert.AreEqual(Color.Black, chessboard.Turn);
         }
-
         [TestMethod]
         public void GetVulnerablePawn_EnPassantNotNull_ShouldReturnTheWhitePawn()
         {
@@ -209,48 +208,6 @@ namespace OpenChess.Tests
 
             IReadOnlyPiece? piece = chessboard.EnPassant.GetVulnerablePawn;
             Assert.IsNull(piece);
-        }
-
-        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/8 b - - 0 1", "F2", "F1", 'q', "Q")]
-        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/4R3 b - - 0 1", "F2", "E1", 'n', "N")]
-        [DataRow("4r3/5P2/8/8/2K5/8/2kR4/8 w - - 0 1", "F7", "E8", 'b', "B")]
-        [DataRow("4r3/5P2/8/8/2K5/8/2kR4/8 w - - 0 1", "F7", "F8", 'r', "R")]
-        [TestMethod]
-        public void MovePiece_ShouldHandlePawnPromotion(string fen, string position1, string position2, char pieceType, string promotingPiece)
-        {
-            Chessboard chessboard = new(fen);
-            Coordinate origin = Coordinate.GetInstance(position1);
-            Coordinate destination = Coordinate.GetInstance(position2);
-            chessboard.MovePiece(origin, destination, promotingPiece);
-            Type? piece = Utils.GetPieceType(pieceType);
-
-            Assert.IsFalse(chessboard.GetReadOnlySquare(origin).HasPiece);
-            Assert.IsTrue(chessboard.GetReadOnlySquare(destination).HasTypeOfPiece(piece!));
-        }
-
-        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/8 b - - 0 1", "F2", "F1", "K")]
-        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/8 b - - 0 1", "F2", "F1", "P")]
-        [TestMethod]
-        public void MovePiece_PromotingPawnToAnInvalidPiece_ShouldThrowException(string fen, string position1, string position2, string promotingPiece)
-        {
-            Chessboard chessboard = new(fen);
-            Coordinate origin = Coordinate.GetInstance(position1);
-            Coordinate destination = Coordinate.GetInstance(position2);
-
-            Assert.ThrowsException<ChessboardException>(() => chessboard.MovePiece(origin, destination, promotingPiece));
-        }
-
-        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/4R3 b - - 0 1", "F2", "E1", null)]
-        [DataRow("4r3/5P2/8/8/2K5/8/2kR4/8 w - - 0 1", "F7", "E8", null)]
-        [TestMethod]
-        public void MovePiece_PromotingPawn_NullString_ShouldPromotePawnToQueen(string fen, string position1, string position2, string promotingPiece)
-        {
-            Chessboard chessboard = new(fen);
-            Coordinate origin = Coordinate.GetInstance(position1);
-            Coordinate destination = Coordinate.GetInstance(position2);
-            chessboard.MovePiece(origin, destination, promotingPiece);
-
-            Assert.IsTrue(chessboard.GetReadOnlySquare(destination).HasTypeOfPiece(typeof(Queen)));
         }
     }
 
--- tests/OpenChess.Domain.Tests/Rules/SpecialMoves/PromotionTests.cs
+++ tests/OpenChess.Domain.Tests/Rules/SpecialMoves/PromotionTests.cs
@@ -5,49 +5,46 @@ namespace OpenChess.Tests
     [TestClass]
     public class PromotionTests
     {
-
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "A2", "A1")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "B2", "B1")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "C2", "C1")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "D2", "D1")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "E2", "E1")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "F2", "F1")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "G2", "G1")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "H2", "H1")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "A7", "A8")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "B7", "B8")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "C7", "C8")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "D7", "D8")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "E7", "E8")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "F7", "F8")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "G7", "G8")]
-        [DataRow("8/PPPPPPPP/8/8/8/8/pppppppp/8 w - - 0 1", "H7", "H8")]
+        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/8 b - - 0 1", "F2", "F1", "K")]
+        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/8 b - - 0 1", "F2", "F1", "P")]
         [TestMethod]
-        public void IsPromoting_ShouldReturnTrue(string fen, string position1, string position2)
+        public void MovePiece_PromotingPawnToAnInvalidPiece_ShouldThrowException(string fen, string position1, string position2, string promotingPiece)
         {
             Chessboard chessboard = new(fen);
-            Promotion promotion = new(chessboard);
             Coordinate origin = Coordinate.GetInstance(position1);
             Coordinate destination = Coordinate.GetInstance(position2);
 
-            Assert.IsTrue(promotion.IsPromoting(origin, destination));
+            Assert.ThrowsException<ChessboardException>(() => chessboard.MovePiece(origin, destination, promotingPiece));
         }
 
-        [DataRow("8/K7/4P3/8/8/4p3/k1q5/8 w - - 0 1", "E6", "E8")]
-        [DataRow("8/K7/4P3/8/8/4p3/k1q5/8 w - - 0 1", "E6", "E7")]
-        [DataRow("8/K7/4P3/8/8/4p3/k1q5/8 w - - 0 1", "E3", "E1")]
-        [DataRow("8/K7/4P3/8/8/4p3/k1q5/8 w - - 0 1", "E3", "E2")]
-        [DataRow("8/K7/4P3/8/8/4p3/k1q5/8 w - - 0 1", "C2", "C1")]
-        [DataRow("8/K7/4P3/8/8/4p3/k1q5/8 w - - 0 1", "A2", "A1")]
-        [DataRow("8/K7/4P3/8/8/4p3/k1q5/8 w - - 0 1", "A7", "A8")]
-        public void IsPromoting_ShouldReturnFalse(string fen, string position1, string position2)
+        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/4R3 b - - 0 1", "F2", "E1", null)]
+        [DataRow("4r3/5P2/8/8/2K5/8/2kR4/8 w - - 0 1", "F7", "E8", null)]
+        [TestMethod]
+        public void MovePiece_PromotingPawn_NullString_ShouldPromotePawnToQueen(string fen, string position1, string position2, string promotingPiece)
         {
             Chessboard chessboard = new(fen);
-            Promotion promotion = new(chessboard);
             Coordinate origin = Coordinate.GetInstance(position1);
             Coordinate destination = Coordinate.GetInstance(position2);
+            chessboard.MovePiece(origin, destination, promotingPiece);
 
-            Assert.IsFalse(promotion.IsPromoting(origin, destination));
+            Assert.IsTrue(chessboard.GetReadOnlySquare(destination).HasTypeOfPiece(typeof(Queen)));
+        }
+
+        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/8 b - - 0 1", "F2", "F1", 'q', "Q")]
+        [DataRow("8/8/8/3r4/2KP4/8/2k2p2/4R3 b - - 0 1", "F2", "E1", 'n', "N")]
+        [DataRow("4r3/5P2/8/8/2K5/8/2kR4/8 w - - 0 1", "F7", "E8", 'b', "B")]
+        [DataRow("4r3/5P2/8/8/2K5/8/2kR4/8 w - - 0 1", "F7", "F8", 'r', "R")]
+        [TestMethod]
+        public void MovePiece_ShouldHandlePawnPromotion(string fen, string position1, string position2, char pieceType, string promotingPiece)
+        {
+            Chessboard chessboard = new(fen);
+            Coordinate origin = Coordinate.GetInstance(position1);
+            Coordinate destination = Coordinate.GetInstance(position2);
+            chessboard.MovePiece(origin, destination, promotingPiece);
+            Type? piece = Utils.GetPieceType(pieceType);
+
+            Assert.IsFalse(chessboard.GetReadOnlySquare(origin).HasPiece);
+            Assert.IsTrue(chessboard.GetReadOnlySquare(destination).HasTypeOfPiece(piece!));
         }
     }
 }
