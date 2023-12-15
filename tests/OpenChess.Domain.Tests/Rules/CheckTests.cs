e040eb5685e0bf9ab5363bd6af48fc98034ea62e
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-15 13:37:31 -0300

    refactor: move fen string to first position in parameter

--- tests/OpenChess.Domain.Tests/Rules/CheckTests.cs
+++ tests/OpenChess.Domain.Tests/Rules/CheckTests.cs
@@ -5,16 +5,16 @@ namespace OpenChess.Tests
     [TestClass]
     public class CheckTests
     {
-        [DataRow('b', "r7/3R2k1/4P3/4K3/8/8/8/8 w - - 0 1")]
-        [DataRow('b', "rn1qkb1r/ppp2pp1/5n1p/1B1p2B1/3P2b1/4P1P1/PP3P1P/RN1QK1NR b KQkq - 0 1")]
-        [DataRow('b', "rn1qkb1r/pp1n2p1/2p2p1p/1B1p2BQ/3P4/4P1P1/PP3P1P/RN2K1NR b KQkq - 0 1")]
-        [DataRow('b', "3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1")]
-        [DataRow('w', "3bk3/8/4Pp2/4K3/8/8/4B3/8 w - - 0 1")]
-        [DataRow('w', "r5K1/3R4/4Pk2/8/8/8/8/8 w - - 0 1")]
-        [DataRow('w', "rnb1kbnr/pppp1ppp/8/8/2B1Pp1q/8/PPPP2PP/RNBQK1NR b KQkq - 0 1")]
-        [DataRow('w', "rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1")]
+        [DataRow("r7/3R2k1/4P3/4K3/8/8/8/8 w - - 0 1", 'b')]
+        [DataRow("rn1qkb1r/ppp2pp1/5n1p/1B1p2B1/3P2b1/4P1P1/PP3P1P/RN1QK1NR b KQkq - 0 1", 'b')]
+        [DataRow("rn1qkb1r/pp1n2p1/2p2p1p/1B1p2BQ/3P4/4P1P1/PP3P1P/RN2K1NR b KQkq - 0 1", 'b')]
+        [DataRow("3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1", 'b')]
+        [DataRow("3bk3/8/4Pp2/4K3/8/8/4B3/8 w - - 0 1", 'w')]
+        [DataRow("r5K1/3R4/4Pk2/8/8/8/8/8 w - - 0 1", 'w')]
+        [DataRow("rnb1kbnr/pppp1ppp/8/8/2B1Pp1q/8/PPPP2PP/RNBQK1NR b KQkq - 0 1", 'w')]
+        [DataRow("rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1", 'w')]
         [TestMethod]
-        public void IsInCheck_PlayerInCheck_ShouldReturnTrue(char color, string fen)
+        public void IsInCheck_PlayerInCheck_ShouldReturnTrue(string fen, char color)
         {
             Chessboard chessboard = new(fen);
             Color player = Utils.ColorFromChar(color);
@@ -22,21 +22,21 @@ namespace OpenChess.Tests
             Assert.IsTrue(Check.IsInCheck(player, chessboard));
         }
 
-        [DataRow('w', "r7/3R2k1/4P3/4K3/8/8/8/8 w - - 0 1")]
-        [DataRow('w', "rn1qkb1r/ppp2pp1/5n1p/1B1p2B1/3P2b1/4P1P1/PP3P1P/RN1QK1NR b KQkq - 0 1")]
-        [DataRow('w', "rn1qkb1r/pp1n2p1/2p2p1p/1B1p2BQ/3P4/4P1P1/PP3P1P/RN2K1NR b KQkq - 0 1")]
-        [DataRow('w', "3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1")]
-        [DataRow('w', "rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")]
-        [DataRow('w', "8/8/8/5PK1/8/k7/8/3r4 w - - 0 1")]
-        [DataRow('b', "3bk3/8/4Pp2/4K3/8/8/4B3/8 w - - 0 1")]
-        [DataRow('b', "r5K1/3R4/4Pk2/8/8/8/8/8 w - - 0 1")]
-        [DataRow('b', "rnb1kbnr/pppp1ppp/8/8/2B1Pp1q/8/PPPP2PP/RNBQK1NR b KQkq - 0 1")]
-        [DataRow('b', "rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1")]
-        [DataRow('b', "rnbqkbnr/pp2pppp/2p5/3P4/3P4/8/PPP2PPP/RNBQKBNR b KQkq - 0 1")]
-        [DataRow('b', "rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")]
-        [DataRow('b', "8/8/8/5PK1/8/k7/8/3r4 w - - 0 1")]
+        [DataRow("r7/3R2k1/4P3/4K3/8/8/8/8 w - - 0 1", 'w')]
+        [DataRow("rn1qkb1r/ppp2pp1/5n1p/1B1p2B1/3P2b1/4P1P1/PP3P1P/RN1QK1NR b KQkq - 0 1", 'w')]
+        [DataRow("rn1qkb1r/pp1n2p1/2p2p1p/1B1p2BQ/3P4/4P1P1/PP3P1P/RN2K1NR b KQkq - 0 1", 'w')]
+        [DataRow("3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1", 'w')]
+        [DataRow("rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1", 'w')]
+        [DataRow("8/8/8/5PK1/8/k7/8/3r4 w - - 0 1", 'w')]
+        [DataRow("3bk3/8/4Pp2/4K3/8/8/4B3/8 w - - 0 1", 'b')]
+        [DataRow("r5K1/3R4/4Pk2/8/8/8/8/8 w - - 0 1", 'b')]
+        [DataRow("rnb1kbnr/pppp1ppp/8/8/2B1Pp1q/8/PPPP2PP/RNBQK1NR b KQkq - 0 1", 'b')]
+        [DataRow("rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1", 'b')]
+        [DataRow("rnbqkbnr/pp2pppp/2p5/3P4/3P4/8/PPP2PPP/RNBQKBNR b KQkq - 0 1", 'b')]
+        [DataRow("rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1", 'b')]
+        [DataRow("8/8/8/5PK1/8/k7/8/3r4 w - - 0 1", 'b')]
         [TestMethod]
-        public void IsInCheck_PlayerNotInCheck_ShouldReturnFalse(char color, string fen)
+        public void IsInCheck_PlayerNotInCheck_ShouldReturnFalse(string fen, char color)
         {
             Chessboard chessboard = new(fen);
             Color player = color == 'w' ? Color.White : Color.Black;
@@ -44,12 +44,12 @@ namespace OpenChess.Tests
             Assert.IsFalse(Check.IsInCheck(player, chessboard));
         }
 
-        [DataRow('w', "3b4/8/4n3/5PK1/8/k4r2/8/3r4 w - - 0 1")]
-        [DataRow('w', "r2qkb1r/1pp2ppp/p2p4/4p3/7b/1B1n1P2/PPPP2PP/RNBQK2R b KQkq - 0 1")]
-        [DataRow('b', "3b4/4n3/8/5PK1/2N5/k4R2/8/3r4 w - - 0 1")]
-        [DataRow('b', "r1bqkbnr/1pp2ppp/p1Bp1N2/4p3/4P3/8/PPPP1PPP/RNBQK2R b KQkq - 0 1")]
+        [DataRow("3b4/8/4n3/5PK1/8/k4r2/8/3r4 w - - 0 1", 'w')]
+        [DataRow("r2qkb1r/1pp2ppp/p2p4/4p3/7b/1B1n1P2/PPPP2PP/RNBQK2R b KQkq - 0 1", 'w')]
+        [DataRow("3b4/4n3/8/5PK1/2N5/k4R2/8/3r4 w - - 0 1", 'b')]
+        [DataRow("r1bqkbnr/1pp2ppp/p1Bp1N2/4p3/4P3/8/PPPP1PPP/RNBQK2R b KQkq - 0 1", 'b')]
         [TestMethod]
-        public void CheckAmount_DoubleCheck_ShouldReturn2(char color, string fen)
+        public void CheckAmount_DoubleCheck_ShouldReturn2(string fen, char color)
         {
             Chessboard chessboard = new(fen);
             Color player = Utils.ColorFromChar(color);
@@ -57,12 +57,12 @@ namespace OpenChess.Tests
             Assert.AreEqual(2, Check.CalculateCheckAmount(player, chessboard));
         }
 
-        [DataRow('b', "rn1qkb1r/ppp2pp1/5n1p/1B1p2B1/3P2b1/4P1P1/PP3P1P/RN1QK1NR b KQkq - 0 1")]
-        [DataRow('w', "rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1")]
-        [DataRow('b', "rn1qkb1r/pp1n2p1/2p2p1p/1B1p2BQ/3P4/4P1P1/PP3P1P/RN2K1NR b KQkq - 0 1")]
-        [DataRow('b', "3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1")]
+        [DataRow("rn1qkb1r/ppp2pp1/5n1p/1B1p2B1/3P2b1/4P1P1/PP3P1P/RN1QK1NR b KQkq - 0 1", 'b')]
+        [DataRow("rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1", 'w')]
+        [DataRow("rn1qkb1r/pp1n2p1/2p2p1p/1B1p2BQ/3P4/4P1P1/PP3P1P/RN2K1NR b KQkq - 0 1", 'b')]
+        [DataRow("3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1", 'b')]
         [TestMethod]
-        public void CheckAmount_Check_ShouldReturn1(char color, string fen)
+        public void CheckAmount_Check_ShouldReturn1(string fen, char color)
         {
             Chessboard chessboard = new(fen);
             Color player = Utils.ColorFromChar(color);
@@ -70,15 +70,15 @@ namespace OpenChess.Tests
             Assert.AreEqual(1, Check.CalculateCheckAmount(player, chessboard));
         }
 
-        [DataRow('w', "3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1")]
-        [DataRow('w', "rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")]
-        [DataRow('w', "8/8/8/5PK1/8/k7/8/3r4 w - - 0 1")]
-        [DataRow('b', "3bk3/8/4Pp2/4K3/8/8/4B3/8 w - - 0 1")]
-        [DataRow('b', "r5K1/3R4/4Pk2/8/8/8/8/8 w - - 0 1")]
-        [DataRow('b', "rnb1kbnr/pppp1ppp/8/8/2B1Pp1q/8/PPPP2PP/RNBQK1NR b KQkq - 0 1")]
-        [DataRow('b', "rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1")]
+        [DataRow("3bk3/5P2/4P3/4K3/8/8/4B3/8 w - - 0 1", 'w')]
+        [DataRow("rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1", 'w')]
+        [DataRow("8/8/8/5PK1/8/k7/8/3r4 w - - 0 1", 'w')]
+        [DataRow("3bk3/8/4Pp2/4K3/8/8/4B3/8 w - - 0 1", 'b')]
+        [DataRow("r5K1/3R4/4Pk2/8/8/8/8/8 w - - 0 1", 'b')]
+        [DataRow("rnb1kbnr/pppp1ppp/8/8/2B1Pp1q/8/PPPP2PP/RNBQK1NR b KQkq - 0 1", 'b')]
+        [DataRow("rnb1k1nr/ppp1qppp/8/8/3N4/1B6/PPP3PP/RNBQK2R b KQkq - 0 1", 'b')]
         [TestMethod]
-        public void CheckAmount_NotInCheck_ShouldReturn0(char color, string fen)
+        public void CheckAmount_NotInCheck_ShouldReturn0(string fen, char color)
         {
             Chessboard chessboard = new(fen);
             Color player = Utils.ColorFromChar(color);
@@ -86,13 +86,13 @@ namespace OpenChess.Tests
             Assert.AreEqual(0, Check.CalculateCheckAmount(player, chessboard));
         }
 
-        [DataRow("E5", "4k3/7R/4P3/2K1r3/8/8/8/8 b - - 0 1")]
-        [DataRow("E5", "4k3/7R/4P3/2K1q3/8/8/8/8 b - - 0 1")]
-        [DataRow("E5", "4k3/7R/3KP3/4b3/8/8/8/8 b - - 0 1")]
-        [DataRow("E5", "4k3/7R/4P3/4n3/2K5/8/8/8 b - - 0 1")]
-        [DataRow("E5", "4k3/7R/4P3/4p3/3K4/8/8/8 b - - 0 1")]
+        [DataRow("4k3/7R/4P3/2K1r3/8/8/8/8 b - - 0 1", "E5")]
+        [DataRow("4k3/7R/4P3/2K1q3/8/8/8/8 b - - 0 1", "E5")]
+        [DataRow("4k3/7R/3KP3/4b3/8/8/8/8 b - - 0 1", "E5")]
+        [DataRow("4k3/7R/4P3/4n3/2K5/8/8/8 b - - 0 1", "E5")]
+        [DataRow("4k3/7R/4P3/4p3/3K4/8/8/8 b - - 0 1", "E5")]
         [TestMethod]
-        public void IsHittingTheEnemyKing_PieceHittingEnemyKing_ShouldReturnTrue(string coordinate, string fen)
+        public void IsHittingTheEnemyKing_PieceHittingEnemyKing_ShouldReturnTrue(string fen, string coordinate)
         {
             Chessboard chessboard = new(fen);
             Coordinate origin = Coordinate.GetInstance(coordinate);
@@ -101,13 +101,13 @@ namespace OpenChess.Tests
             Assert.IsTrue(Check.IsHittingTheEnemyKing(piece, chessboard));
         }
 
-        [DataRow("E5", "4k3/7R/4P3/4p3/4K3/8/8/8 b - - 0 1")]
-        [DataRow("E5", "4k3/7R/4P3/4b3/4K3/8/8/8 b - - 0 1")]
-        [DataRow("E5", "4k3/7R/4P3/4n3/4K3/8/8/8 b - - 0 1")]
-        [DataRow("C3", "4k3/7R/4P3/8/4K3/2q5/8/8 b - - 0 1")]
-        [DataRow("C3", "4k3/7R/4P3/8/4K3/2r5/8/8 b - - 0 1")]
+        [DataRow("4k3/7R/4P3/4p3/4K3/8/8/8 b - - 0 1", "E5")]
+        [DataRow("4k3/7R/4P3/4b3/4K3/8/8/8 b - - 0 1", "E5")]
+        [DataRow("4k3/7R/4P3/4n3/4K3/8/8/8 b - - 0 1", "E5")]
+        [DataRow("4k3/7R/4P3/8/4K3/2q5/8/8 b - - 0 1", "C3")]
+        [DataRow("4k3/7R/4P3/8/4K3/2r5/8/8 b - - 0 1", "C3")]
         [TestMethod]
-        public void IsHittingTheEnemyKing_PieceNotHittingEnemyKing_ShouldReturnFalse(string coordinate, string fen)
+        public void IsHittingTheEnemyKing_PieceNotHittingEnemyKing_ShouldReturnFalse(string fen, string coordinate)
         {
             Chessboard chessboard = new(fen);
             Coordinate origin = Coordinate.GetInstance(coordinate);
--- tests/OpenChess.Domain.Tests/Rules/SpecialMoves/EnPassantTests.cs
+++ tests/OpenChess.Domain.Tests/Rules/SpecialMoves/EnPassantTests.cs
@@ -5,10 +5,10 @@ namespace OpenChess.Tests
     [TestClass]
     public class EnPassantTests
     {
-        [DataRow("D4", "E3", "rnbqkbnr/pppp1ppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR b KQkq E3 0 1", 'w', 'b')]
-        [DataRow("D5", "E6", "rnbqkbnr/pppp1ppp/8/3Pp3/8/8/PPP1PPPP/RNBQKBNR w KQkq E6 0 1", 'b', 'w')]
+        [DataRow("rnbqkbnr/pppp1ppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR b KQkq E3 0 1", "D4", "E3", 'w', 'b')]
+        [DataRow("rnbqkbnr/pppp1ppp/8/3Pp3/8/8/PPP1PPPP/RNBQKBNR w KQkq E6 0 1", "D5", "E6", 'b', 'w')]
         [TestMethod]
-        public void MovePiece_PawnVulnerable_ShouldBeCapturedByEnPassant(string position1, string position2, string fen, char color1, char color2)
+        public void MovePiece_PawnVulnerable_ShouldBeCapturedByEnPassant(string fen, string position1, string position2, char color1, char color2)
         {
             Chessboard chessboard = new(fen);
             var origin = Coordinate.GetInstance(position1);
@@ -22,10 +22,10 @@ namespace OpenChess.Tests
             Assert.AreEqual(chessboard.GetReadOnlySquare(destination).ReadOnlyPiece!.Color, Utils.ColorFromChar(color2));
         }
 
-        [DataRow("D4", "E3", "rnbqkbnr/pppp1ppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")]
-        [DataRow("D5", "E6", "rnbqkbnr/pppp1ppp/8/3Pp3/8/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1")]
+        [DataRow("rnbqkbnr/pppp1ppp/8/8/3pP3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1", "D4", "E3")]
+        [DataRow("rnbqkbnr/pppp1ppp/8/3Pp3/8/8/PPP1PPPP/RNBQKBNR w KQkq - 0 1", "D5", "E6")]
         [TestMethod]
-        public void MovePiece_PawnNotVulnerable_ShouldNotBeCaptured(string position1, string position2, string fen)
+        public void MovePiece_PawnNotVulnerable_ShouldNotBeCaptured(string fen, string position1, string position2)
         {
             Chessboard chessboard = new(fen);
             var origin = Coordinate.GetInstance(position1);
@@ -34,10 +34,10 @@ namespace OpenChess.Tests
             Assert.ThrowsException<ChessboardException>(() => chessboard.MovePiece(origin, destination));
         }
 
-        [DataRow("D7", "E4", "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq E3 0 1")]
-        [DataRow("D2", "E5", "rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq E6 0 1")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq E3 0 1", "D7", "E4")]
+        [DataRow("rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq E6 0 1", "D2", "E5")]
         [TestMethod]
-        public void MovePiece_PawnOutOfRange_ShouldNotBeAbleToCapture(string position1, string position2, string fen)
+        public void MovePiece_PawnOutOfRange_ShouldNotBeAbleToCapture(string fen, string position1, string position2)
         {
             Chessboard chessboard = new(fen);
             var origin = Coordinate.GetInstance(position1);
