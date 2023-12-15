91233f7fdb2aa0336883d5daa158004d9f071797
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-15 18:52:38 -0300

    feat: parse color from char

--- src/OpenChess.Domain/Chessboard/Color.cs
+++ src/OpenChess.Domain/Chessboard/Color.cs
@@ -12,5 +12,12 @@ namespace OpenChess.Domain
         {
             return color == Color.Black ? Color.White : Color.Black;
         }
+
+        public static Color TryParseColor(char color)
+        {
+            bool colorExists = Enum.IsDefined(typeof(Color), (int)color);
+            if (!colorExists) throw new MatchException($"Could not cast the value {color} to a color.");
+            return (Color)color;
+        }
     }
 }
