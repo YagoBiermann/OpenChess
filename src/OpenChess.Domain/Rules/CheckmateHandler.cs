74f27935ecc8c8ada74a9ec6998ae3bcab6212f4
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-20 22:42:17 -0300

    feat: add method to check if it is checkmate

--- src/OpenChess.Domain/Rules/CheckmateHandler.cs
+++ src/OpenChess.Domain/Rules/CheckmateHandler.cs
@@ -10,5 +10,20 @@ namespace OpenChess.Domain
             _checkHandler = new(chessboard);
         }
 
+        public bool IsCheckmate()
+        {
+            int checkAmount = _checkHandler.CalculateCheckAmount(_chessboard.Turn);
+            bool isNotInCheck = checkAmount == 0;
+            bool isInCheck = checkAmount == 1;
+            bool isInDoubleCheck = checkAmount >= 2;
+            if (isNotInCheck) return false;
+            if (isInDoubleCheck) return CanSolveCheckByMovingTheKing();
+            if (isInCheck) return CanSolveCheckByCoveringTheKing() || CanSolveCheckByMovingAPiece() || CanSolveCheckByMovingTheKing();
+            return true;
+        }
+
+        private bool CanSolveCheckByMovingAPiece() { throw new Exception("Not implemented yet"); }
+        private bool CanSolveCheckByCoveringTheKing() { throw new Exception("Not implemented yet"); }
+        private bool CanSolveCheckByMovingTheKing() { throw new Exception("Not implemented yet"); }
     }
 }
