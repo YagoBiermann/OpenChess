d4f6ee1338697d08c1a75f7fdb85bf8b042224aa
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-22 16:50:29 -0300

    test: ensure that default pawn move is converted to PGN move correctly

--- tests/OpenChess.Domain.Tests/Match/PGNBuilderTests.cs
+++ tests/OpenChess.Domain.Tests/Match/PGNBuilderTests.cs
@@ -5,5 +5,15 @@ namespace OpenChess.Tests
     [TestClass]
     public class PGNBuilderTests
     {
+        [DataRow("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "E2", "E4", "1. e4")]
+        [DataRow("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1", "E7", "E5", "1. e5")]
+        [DataRow("rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1", "D2", "D4", "1. d4")]
+        [TestMethod]
+        public void PawnMove_Default_ShouldAddPGNInCorrectFormat(string fen, string origin, string destination, string expectedPGNMove)
+        {
+            Match match = FakeMatch.RestoreAndPlay(fen, origin, destination);
+
+            Assert.AreEqual(expectedPGNMove, match.Moves.Peek());
+        }
     }
 }
