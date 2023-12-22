1f1dafe64e2e2194413804c0da690ed2b047b1e1
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-21 22:47:02 -0300

    refactor: implement the new interface

--- src/OpenChess.Domain/Rules/MovesCalculator/LegalMoveStrategy.cs
+++ src/OpenChess.Domain/Rules/MovesCalculator/LegalMoveStrategy.cs
@@ -3,24 +3,11 @@ namespace OpenChess.Domain
 {
     internal class LegalMoveStrategy : IMoveCalculatorStrategy
     {
-        public List<Coordinate> Calculate(IReadOnlyChessboard chessboard, IReadOnlyPiece piece, MoveDirections move)
+        public bool ShouldIncludePiece(Color player, IReadOnlyPiece pieceAtLastPosition)
         {
-            List<Coordinate> legalMoves = new(move.Coordinates);
-            if (!legalMoves.Any()) return legalMoves;
+            bool dontIncludeAllyPiece = !(pieceAtLastPosition.Color == player);
 
-            IReadOnlySquare square = chessboard.GetReadOnlySquare(legalMoves.Last());
-            int lastPosition = legalMoves.IndexOf(legalMoves.Last());
-            if (piece is not Pawn && !square.HasPiece) { return legalMoves; }
-            Coordinate? enPassant = chessboard.EnPassantAvailability.EnPassantPosition;
-            bool squareHasAllyPiece = !square.HasEnemyPiece(piece.Color);
-            bool isEnPassant = square.Coordinate.Equals(enPassant);
-            bool squareIsEmptyAndIsNotEnPassant = !square.HasPiece && !isEnPassant;
-            bool isForwardMove = piece is Pawn pawn && move.Direction.Equals(pawn.ForwardDirection);
-
-            if (piece is Pawn && (squareHasAllyPiece || squareIsEmptyAndIsNotEnPassant || isForwardMove)) legalMoves.RemoveAt(lastPosition);
-            if (piece is not Pawn && squareHasAllyPiece) legalMoves.RemoveAt(lastPosition);
-
-            return legalMoves;
+            return dontIncludeAllyPiece;
         }
     }
 }
