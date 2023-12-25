ff4bb0c0ffb8f336ccdbb1c84f220c0866c65165
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-25 17:48:14 -0300

    refactor: improve test case

--- tests/OpenChess.Domain.Tests/Rules/CheckmateTests.cs
+++ tests/OpenChess.Domain.Tests/Rules/CheckmateTests.cs
@@ -5,14 +5,15 @@ namespace OpenChess.Tests
     [TestClass]
     public class CheckmateTests
     {
-        [DataRow("2N5/k7/8/2Q5/7p/8/8/4K3 b - - 0 1", "A7", "B7")]
-        [DataRow("8/k1R5/8/8/3B3p/8/8/4K3 b - - 0 1", "A7", "B8")]
+        [DataRow("2N5/k7/8/2Q5/7p/8/8/4K3 b - - 0 1", "A7", "B7", "DoubleCheck")]
+        [DataRow("8/k1R5/8/8/3B3p/8/8/4K3 b - - 0 1", "A7", "B8", "DoubleCheck")]
+        [DataRow("8/8/kP6/8/8/3BK3/7p/8 b - - 0 1", "A6", "A5", "Check")]
+        [DataRow("8/8/kP6/8/8/3BK3/7p/8 b - - 0 1", "A6", "B7", "Check")]
         [TestMethod]
-        public void DoubleCheck_ShouldBeSolvedByMovingTheKing(string fen, string origin, string destination)
+        public void Check_ShouldBeSolvedByMovingTheKing(string fen, string origin, string destination, string checkState)
         {
-            MatchInfo matchInfo = FakeMatch.RestoreMatch(fen, "DoubleCheck");
+            MatchInfo matchInfo = FakeMatch.RestoreMatch(fen, checkState);
             Match match = new(matchInfo);
-            Assert.AreEqual(CheckState.DoubleCheck, match.CurrentPlayerCheckState);
 
             Move move = new(match.CurrentPlayer!.Value.Id, Coordinate.GetInstance(origin), Coordinate.GetInstance(destination));
             match.Play(move);
@@ -20,14 +21,14 @@ namespace OpenChess.Tests
             Assert.AreEqual(CheckState.NotInCheck, match.CurrentPlayerCheckState);
         }
 
-        [DataRow("8/kR6/8/8/3B3p/8/8/4K3 b - - 0 1", "A7", "B7")]
-        [DataRow("8/2k5/2R5/Q7/7p/8/8/4K3 b - - 0 1", "C7", "C6")]
+        [DataRow("8/kR6/8/8/3B3p/8/8/4K3 b - - 0 1", "A7", "B7", "DoubleCheck")]
+        [DataRow("8/2k5/2R5/Q7/7p/8/8/4K3 b - - 0 1", "C7", "C6", "DoubleCheck")]
+        [DataRow("8/8/kP6/8/8/3BK3/7p/8 b - - 0 1", "A6", "B6", "DoubleCheck")]
         [TestMethod]
-        public void DoubleCheck_ShouldBeSolvedByCapturingAPiece(string fen, string origin, string destination)
+        public void Check_ShouldBeSolvedByCapturingAPieceWithTheKing(string fen, string origin, string destination, string checkState)
         {
-            MatchInfo matchInfo = FakeMatch.RestoreMatch(fen, "DoubleCheck");
+            MatchInfo matchInfo = FakeMatch.RestoreMatch(fen, checkState);
             Match match = new(matchInfo);
-            Assert.AreEqual(CheckState.DoubleCheck, match.CurrentPlayerCheckState);
 
             Move move = new(match.CurrentPlayer!.Value.Id, Coordinate.GetInstance(origin), Coordinate.GetInstance(destination));
             match.Play(move);
