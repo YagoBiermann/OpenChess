415736dfa73f05fc43d98fc331ad2a52c23787eb
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-24 22:38:56 -0300

    test: ensure that double check can be solved by capturing a piece

--- tests/OpenChess.Domain.Tests/Rules/CheckmateTests.cs
+++ tests/OpenChess.Domain.Tests/Rules/CheckmateTests.cs
@@ -19,5 +19,21 @@ namespace OpenChess.Tests
 
             Assert.AreEqual(CheckState.NotInCheck, match.CurrentPlayerCheckState);
         }
+
+        [DataRow("8/kR6/8/8/3B3p/8/8/4K3 b - - 0 1", "A7", "B7")]
+        [DataRow("8/2k5/2R5/Q7/7p/8/8/4K3 b - - 0 1", "C7", "C6")]
+        [TestMethod]
+        public void DoubleCheck_ShouldBeSolvedByCapturingAPiece(string fen, string origin, string destination)
+        {
+            MatchInfo matchInfo = FakeMatch.RestoreMatch(fen, "DoubleCheck");
+            Match match = new(matchInfo);
+            Assert.AreEqual(CheckState.DoubleCheck, match.CurrentPlayerCheckState);
+
+            Move move = new(match.CurrentPlayer!.Value.Id, Coordinate.GetInstance(origin), Coordinate.GetInstance(destination));
+            match.Play(move);
+
+            Assert.AreEqual(CheckState.NotInCheck, match.CurrentPlayerCheckState);
+        }
+        
     }
 }
