f2ac49bab52fbbfd78b67daf59c2c2705a21e0dd
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-24 21:13:39 -0300

    test: ensure that double check can be solved by moving the king

--- tests/OpenChess.Domain.Tests/Rules/CheckmateTests.cs
+++ tests/OpenChess.Domain.Tests/Rules/CheckmateTests.cs
@@ -1,8 +1,23 @@
+using OpenChess.Domain;
+
 namespace OpenChess.Tests
 {
     [TestClass]
     public class CheckmateTests
     {
+        [DataRow("2N5/k7/8/2Q5/7p/8/8/4K3 b - - 0 1", "A7", "B7")]
+        [DataRow("8/k1R5/8/8/3B3p/8/8/4K3 b - - 0 1", "A7", "B8")]
+        [TestMethod]
+        public void DoubleCheck_ShouldBeSolvedByMovingTheKing(string fen, string origin, string destination)
+        {
+            MatchInfo matchInfo = FakeMatch.RestoreMatch(fen, "DoubleCheck");
+            Match match = new(matchInfo);
+            Assert.AreEqual(CheckState.DoubleCheck, match.CurrentPlayerCheckState);
 
+            Move move = new(match.CurrentPlayer!.Value.Id, Coordinate.GetInstance(origin), Coordinate.GetInstance(destination));
+            match.Play(move);
+
+            Assert.AreEqual(CheckState.NotInCheck, match.CurrentPlayerCheckState);
+        }
     }
 }
