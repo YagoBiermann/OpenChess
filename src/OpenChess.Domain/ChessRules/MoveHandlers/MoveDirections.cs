1a3998debda03d0d1c623b3f510bcf96913c0a4e
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-28 23:03:59 -0300

    refactor: add properties needed to further calculations

--- src/OpenChess.Domain/ChessRules/MoveHandlers/MoveDirections.cs
+++ src/OpenChess.Domain/ChessRules/MoveHandlers/MoveDirections.cs
@@ -4,13 +4,19 @@ namespace OpenChess.Domain
     {
         public IReadOnlyPiece Piece { get; }
         public Direction Direction { get; }
-        public List<Coordinate> Coordinates { get; }
+        public List<Coordinate>? FullRange { get; }
+        public List<Coordinate>? RangeOfAttack { get; }
+        public IReadOnlyPiece? NearestPiece { get; }
+        public List<CoordinateDistances>? PiecesWithDistanceFromOrigin { get; }
 
-        public MoveDirections(Direction direction, List<Coordinate> coordinates, IReadOnlyPiece piece)
+        public MoveDirections(IReadOnlyPiece piece, Direction direction, List<Coordinate>? fullMoveRange = null, List<Coordinate>? attackingMoveRange = null, List<CoordinateDistances>? allPiecesInMoveRange = null, IReadOnlyPiece? nearestPiece = null)
         {
-            Direction = direction;
-            Coordinates = coordinates;
             Piece = piece;
+            Direction = direction;
+            FullRange = fullMoveRange;
+            RangeOfAttack = attackingMoveRange;
+            PiecesWithDistanceFromOrigin = allPiecesInMoveRange;
+            NearestPiece = nearestPiece;
         }
     }
 }
