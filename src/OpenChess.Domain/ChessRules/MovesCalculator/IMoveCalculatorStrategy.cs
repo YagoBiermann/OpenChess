6aca1cf9d8fc3373511a3fefb663e0408ba39b10
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-28 23:51:50 -0300

    refactor: remove unnecessary class

--- src/OpenChess.Domain/ChessRules/MovesCalculator/IMoveCalculatorStrategy.cs
+++
@@ -1,7 +0,0 @@
-namespace OpenChess.Domain
-{
-    internal interface IMoveCalculatorStrategy
-    {
-        public bool ShouldIncludePiece(Color player, IReadOnlyPiece pieceAtLastPosition);
-    }
-}
