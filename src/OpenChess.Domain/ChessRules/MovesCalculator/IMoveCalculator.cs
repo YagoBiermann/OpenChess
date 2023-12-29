f6eb8d01af5062faa86448b84af02861b6c977ca
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-29 14:52:14 -0300

    refactor: update interface

--- src/OpenChess.Domain/ChessRules/MovesCalculator/IMoveCalculator.cs
+++ src/OpenChess.Domain/ChessRules/MovesCalculator/IMoveCalculator.cs
@@ -2,6 +2,11 @@ namespace OpenChess.Domain
 {
     internal interface IMoveCalculator
     {
+        public bool PieceCanSolveTheCheck(IReadOnlyPiece piece);
+        public bool CanMoveToPosition(IReadOnlyPiece piece, Coordinate destination);
+        public bool IsHittingTheEnemyKing(IReadOnlyPiece piece);
+        public List<MoveDirections> CalculateMovesHittingTheEnemyKing(Color player);
+        public void CalculateAndCacheAllMoves();
         public List<MoveDirections> CalculateMoves(IReadOnlyPiece piece);
     }
 }
