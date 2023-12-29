abc7636a4e6bc42d5dbd7d479a56957b6a404218
YagoBiermann <bsfyagobsf@gmail.com>
2023-12-28 23:20:32 -0300

    refactor: update references

--- src/OpenChess.Domain/ChessRules/MovesCalculator/MovesCalculator.cs
+++ src/OpenChess.Domain/ChessRules/MovesCalculator/MovesCalculator.cs
@@ -45,13 +45,13 @@ namespace OpenChess.Domain
             foreach (MoveDirections move in fullMoveRange)
             {
                 Direction currentDirection = move.Direction;
-                List<Coordinate> piecesPosition = _chessboard.GetPiecesPosition(move.Coordinates);
                 if (move.FullRange is null)
                 {
                     MoveDirections pawnMoveRange = CreateMoveRange(piece, currentDirection);
                     legalMoves.Add(pawnMoveRange);
                     continue;
                 }
+                List<Coordinate> piecesPosition = _chessboard.GetPiecesPosition(move.FullRange!);
                 List<Coordinate> rangeOfAttack = RangeOfAttack(piece, piecesPosition, move);
                 bool lastPositionIsEmpty = !_chessboard.GetReadOnlySquare(rangeOfAttack.Last()).HasPiece;
                 if (piece is not Pawn && lastPositionIsEmpty)
@@ -92,17 +92,17 @@ namespace OpenChess.Domain
 
         private static List<Coordinate> RangeOfAttack(IReadOnlyPiece piece, List<Coordinate> piecesPosition, MoveDirections move)
         {
-            if (!piecesPosition.Any()) return new(move.Coordinates);
+            if (!piecesPosition.Any()) return new(move.FullRange!);
             List<CoordinateDistances> distances = CoordinateDistances.CalculateDistance(piece.Origin, piecesPosition);
             CoordinateDistances nearestPiece = CoordinateDistances.CalculateNearestDistance(distances);
-            List<Coordinate> rangeOfAttack = move.Coordinates.Take(nearestPiece.DistanceBetween).ToList();
+            List<Coordinate> rangeOfAttack = move.FullRange!.Take(nearestPiece.DistanceBetween).ToList();
 
             return rangeOfAttack;
         }
 
         private bool SpecialPawnRuleApplies(MoveDirections move, Pawn pawn, List<Coordinate> piecesPosition, bool lastPositionIsEmpty)
         {
-            bool isNotEnPassantPosition = !move.Coordinates.Contains(_chessboard.EnPassantAvailability.EnPassantPosition!);
+            bool isNotEnPassantPosition = !move.FullRange!.Contains(_chessboard.EnPassantAvailability.EnPassantPosition!);
             bool isForwardMove = move.Direction.Equals(pawn.ForwardDirection);
             bool isEmptyDiagonal = !piecesPosition.Any() && !isForwardMove;
             bool isForwardMoveAndHasPiece = !lastPositionIsEmpty && isForwardMove;
