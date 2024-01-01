22df22b2f300801980ed9118684aea598e89ecd0
YagoBiermann <bsfyagobsf@gmail.com>
2024-01-01 13:22:38 -0300

    refactor: update reference

--- tests/OpenChess.Domain.Tests/Chessboard/Pieces/ExpectedMoves.cs
+++ tests/OpenChess.Domain.Tests/Chessboard/Pieces/ExpectedMoves.cs
@@ -7,8 +7,8 @@ namespace OpenChess.Tests
         internal static PieceLineOfSight GetLineOfSight(IReadOnlyChessboard chessboard, IReadOnlyPiece piece, Direction direction, int amount)
         {
             var lineOfSight = Coordinate.CalculateSequence(piece.Origin, direction, amount);
-            var allPiecesInLineOfSight = chessboard.GetPieces(lineOfSight);
-            PieceLineOfSight move = new(piece, direction, lineOfSight, allPiecesInLineOfSight);
+            var piecesInLineOfSight = PieceDistances.CalculateDistance(piece, chessboard.GetPieces(lineOfSight));
+            PieceLineOfSight move = new(piece, direction, lineOfSight, piecesInLineOfSight);
 
             return move;
         }
