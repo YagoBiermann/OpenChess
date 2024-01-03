8c5fc757b761494248e070b8f43917de70d2c1fe
YagoBiermann <bsfyagobsf@gmail.com>
2024-01-03 00:06:47 -0300

    refactor: loop through dictionary to determine castling availability instead of checking each boolean individually

--- src/OpenChess.Domain/Chessboard/CastlingAvailability.cs
+++ src/OpenChess.Domain/Chessboard/CastlingAvailability.cs
@@ -2,6 +2,7 @@ namespace OpenChess.Domain
 {
     internal struct CastlingAvailability : ICastlingAvailability
     {
+        private Dictionary<string, List<char>> _castlingPiecesPosition { get; }
         public Dictionary<char, bool> IsAvailableAt { get; private set; }
         public static char WhiteKingSide = 'K';
         public static char WhiteQueenSide = 'Q';
@@ -21,26 +22,34 @@ namespace OpenChess.Domain
         public CastlingAvailability(Dictionary<char, bool> castlingAvailability)
         {
             IsAvailableAt = castlingAvailability;
+            _castlingPiecesPosition = new()
+            {
+                {"E1", new() {'K','Q'}},
+                {"A1", new() {'Q'}},
+                {"H1", new() {'K'}},
+                {"E8", new() {'k', 'q'}},
+                {"H8", new() {'k'}},
+                {"A8", new() {'q'}},
+            };
         }
 
         public void UpdateAvailability(Coordinate origin, Color player)
         {
-            if (player is Color.White && origin.Equals(Coordinate.GetInstance("E1"))) { IsWhiteKingSideAvailable = false; IsWhiteQueenSideAvailable = false; }
-            if (player is Color.White && origin.Equals(Coordinate.GetInstance("A1"))) { IsWhiteQueenSideAvailable = false; };
-            if (player is Color.White && origin.Equals(Coordinate.GetInstance("H1"))) { IsWhiteKingSideAvailable = false; };
-            if (player is Color.Black && origin.Equals(Coordinate.GetInstance("E8"))) { IsBlackKingSideAvailable = false; IsBlackQueenSideAvailable = false; }
-            if (player is Color.Black && origin.Equals(Coordinate.GetInstance("A8"))) { IsBlackQueenSideAvailable = false; };
-            if (player is Color.Black && origin.Equals(Coordinate.GetInstance("H8"))) { IsBlackKingSideAvailable = false; };
+            Dictionary<char, bool> newCastlingAvailability = IsAvailableAt;
+            _castlingPiecesPosition[origin.ToString()].ForEach(v => { newCastlingAvailability[v] = false; });
+            IsAvailableAt = newCastlingAvailability;
         }
 
-        public override string ToString()
+        public override readonly string ToString()
         {
             string castlingAvailability = "";
-            if (IsWhiteKingSideAvailable) castlingAvailability += "K";
-            if (IsWhiteQueenSideAvailable) castlingAvailability += "Q";
-            if (IsBlackKingSideAvailable) castlingAvailability += "k";
-            if (IsBlackQueenSideAvailable) castlingAvailability += "q";
-            if (!IsWhiteKingSideAvailable && !IsWhiteQueenSideAvailable && !IsBlackKingSideAvailable && !IsBlackQueenSideAvailable) castlingAvailability += "-";
+
+            foreach (var kv in IsAvailableAt)
+            {
+                bool isAvailable = kv.Value;
+                char castling = kv.Key;
+                if (isAvailable) { castlingAvailability += castling; }
+            }
 
             return castlingAvailability;
         }
