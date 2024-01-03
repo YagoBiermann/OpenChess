3ad55badbb9be64625e4b245699714d6d4b40f66
YagoBiermann <bsfyagobsf@gmail.com>
2024-01-02 23:49:00 -0300

    refactor: remove boolean properties

--- src/OpenChess.Domain/Chessboard/CastlingAvailability.cs
+++ src/OpenChess.Domain/Chessboard/CastlingAvailability.cs
@@ -2,11 +2,6 @@ namespace OpenChess.Domain
 {
     internal struct CastlingAvailability : ICastlingAvailability
     {
-        public bool IsWhiteKingSideAvailable { get; private set; }
-        public bool IsWhiteQueenSideAvailable { get; private set; }
-        public bool IsBlackKingSideAvailable { get; private set; }
-        public bool IsBlackQueenSideAvailable { get; private set; }
-
         public Dictionary<char, bool> IsAvailableAt { get; private set; }
         public static char WhiteKingSide = 'K';
         public static char WhiteQueenSide = 'Q';
@@ -21,24 +16,11 @@ namespace OpenChess.Domain
                 {'k', true},
                 {'q', true},
             };
-
-            IsWhiteKingSideAvailable = true;
-            IsWhiteQueenSideAvailable = true;
-            IsBlackKingSideAvailable = true;
-            IsBlackQueenSideAvailable = true;
         }
 
         public CastlingAvailability(Dictionary<char, bool> castlingAvailability)
         {
             IsAvailableAt = castlingAvailability;
-        }
-
-        public CastlingAvailability(bool whiteKingSide, bool whiteQueenSide, bool blackKingSide, bool blackQueenSide)
-        {
-            IsWhiteKingSideAvailable = whiteKingSide;
-            IsWhiteQueenSideAvailable = whiteQueenSide;
-            IsBlackKingSideAvailable = blackKingSide;
-            IsBlackQueenSideAvailable = blackQueenSide;
         }
 
         public void UpdateAvailability(Coordinate origin, Color player)
