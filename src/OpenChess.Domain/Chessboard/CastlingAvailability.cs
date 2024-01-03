22af576eb75ba52971aab211662a23aad6790c48
YagoBiermann <bsfyagobsf@gmail.com>
2024-01-03 00:15:07 -0300

    refactor: add constructor with boolean parameters

--- src/OpenChess.Domain/Chessboard/CastlingAvailability.cs
+++ src/OpenChess.Domain/Chessboard/CastlingAvailability.cs
@@ -2,35 +2,34 @@ namespace OpenChess.Domain
 {
     internal struct CastlingAvailability : ICastlingAvailability
     {
-        private Dictionary<string, List<char>> _castlingPiecesPosition { get; }
-        public Dictionary<char, bool> IsAvailableAt { get; private set; }
+        private Dictionary<string, List<char>> _castlingPiecesPosition = new()
+        {
+            {"E1", new() {'K','Q'}},
+            {"A1", new() {'Q'}},
+            {"H1", new() {'K'}},
+            {"E8", new() {'k', 'q'}},
+            {"H8", new() {'k'}},
+            {"A8", new() {'q'}},
+        };
+        public Dictionary<char, bool> IsAvailableAt { get; private set; } = new()
+        {
+            {'K', true},
+            {'Q', true},
+            {'k', true},
+            {'q', true},
+        };
         public static char WhiteKingSide = 'K';
         public static char WhiteQueenSide = 'Q';
         public static char BlackKingSide = 'k';
         public static char BlackQueenSide = 'q';
-        public CastlingAvailability()
-        {
-            IsAvailableAt = new()
-            {
-                {'K', true},
-                {'Q', true},
-                {'k', true},
-                {'q', true},
-            };
-        }
+        public CastlingAvailability() { }
 
-        public CastlingAvailability(Dictionary<char, bool> castlingAvailability)
+        public CastlingAvailability(bool whiteKingSide, bool whiteQueenSide, bool blackKingSide, bool blackQueenSide)
         {
-            IsAvailableAt = castlingAvailability;
-            _castlingPiecesPosition = new()
-            {
-                {"E1", new() {'K','Q'}},
-                {"A1", new() {'Q'}},
-                {"H1", new() {'K'}},
-                {"E8", new() {'k', 'q'}},
-                {"H8", new() {'k'}},
-                {"A8", new() {'q'}},
-            };
+            IsAvailableAt['K'] = whiteKingSide;
+            IsAvailableAt['Q'] = whiteQueenSide;
+            IsAvailableAt['k'] = blackKingSide;
+            IsAvailableAt['q'] = blackQueenSide;
         }
 
         public void UpdateAvailability(Coordinate origin, Color player)
