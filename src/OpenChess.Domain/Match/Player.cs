9ece0919c3218b3cb346e71af4d676697314d59d
YagoBiermann <bsfyagobsf@gmail.com>
2024-01-03 18:37:06 -0300

    feat: add new property

--- src/OpenChess.Domain/Match/Player.cs
+++ src/OpenChess.Domain/Match/Player.cs
@@ -5,12 +5,14 @@ namespace OpenChess.Domain
         public Guid Id { get; }
         public Color Color { get; }
         public Guid? CurrentMatch { get; private set; }
+        public bool IsCurrentPlayer { get; set; }
 
         public Player(PlayerInfo info)
         {
             Id = info.Id;
             Color = info.Color;
             CurrentMatch = info.CurrentMatch;
+            IsCurrentPlayer = false;
         }
 
         public void Join(Guid match)
