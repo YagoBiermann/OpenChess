65e6b19243052bc8ff7004d7de48c9abdfa16f9b
YagoBiermann <bsfyagobsf@gmail.com>
2024-01-04 09:30:37 -0300

    feat: add equals override

--- src/OpenChess.Domain/Match/Player.cs
+++ src/OpenChess.Domain/Match/Player.cs
@@ -30,5 +30,19 @@ namespace OpenChess.Domain
         {
             get { return new(Id, Color, CurrentMatch); }
         }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Player player = (Player)obj;
+            bool areEqual = player.Color == Color && player.Id == Id && player.CurrentMatch == CurrentMatch && IsCurrentPlayer == player.IsCurrentPlayer;
+
+            return areEqual;
+        }
+
+        public override int GetHashCode() => (Id, CurrentMatch, Color, IsCurrentPlayer).GetHashCode();
     }
 }
