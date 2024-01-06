beb420a65691ac651c85818a94d014cd2caf9346
YagoBiermann <bsfyagobsf@gmail.com>
2024-01-06 16:35:04 -0300

    feat: add Color property to square interface

--- src/OpenChess.Domain/Chessboard/Interfaces/IReadOnlySquare.cs
+++ src/OpenChess.Domain/Chessboard/Interfaces/IReadOnlySquare.cs
@@ -4,6 +4,7 @@ namespace OpenChess.Domain
     {
         public IReadOnlyPiece? ReadOnlyPiece { get; }
         public Coordinate Coordinate { get; }
+        public Color Color { get; }
         public bool HasPiece { get; }
         public bool HasEnemyPiece(Color currentPlayer);
     }
