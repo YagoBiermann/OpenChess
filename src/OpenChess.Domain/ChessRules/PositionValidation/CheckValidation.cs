72cbc6b5300fb14d0b3ea1b45a22f3cbfbfa0df1
YagoBiermann <bsfyagobsf@gmail.com>
2024-01-06 17:18:45 -0300

    refactor: move check validation from checkHandler into CheckValidation

--- src/OpenChess.Domain/ChessRules/PositionValidation/CheckValidation.cs
+++ src/OpenChess.Domain/ChessRules/PositionValidation/CheckValidation.cs
@@ -7,9 +7,37 @@ namespace OpenChess.Domain
         }
         public override CurrentPositionStatus ValidatePosition(CurrentPositionStatus? checkState = null)
         {
-            CheckHandler checkHandler = new(_match.Chessboard, _movesCalculator);
-            checkHandler.IsInCheck(_match.OpponentPlayerColor!.Value, out CurrentPositionStatus status);
+            IsInCheck(_match.OpponentPlayerColor!.Value, out CurrentPositionStatus status);
             return base.ValidatePosition(status);
+        }
+
+        public bool IsInCheck(Color player, out CurrentPositionStatus checkState)
+        {
+            int checkAmount = CalculateCheckAmount(player);
+            checkState = GetCheckState(checkAmount);
+
+            return checkState != CurrentPositionStatus.NotInCheck;
+        }
+
+        private int CalculateCheckAmount(Color player)
+        {
+            List<IReadOnlyPiece> pieces = _match.Chessboard.GetPieces(ColorUtils.GetOppositeColor(player));
+            int checkAmount = 0;
+
+            foreach (IReadOnlyPiece piece in pieces) { if (_movesCalculator.IsHittingTheEnemyKing(piece)) { checkAmount++; }; }
+
+            return checkAmount;
+        }
+
+        private static CurrentPositionStatus GetCheckState(int checkAmount)
+        {
+            return checkAmount switch
+            {
+                0 => CurrentPositionStatus.NotInCheck,
+                1 => CurrentPositionStatus.Check,
+                2 => CurrentPositionStatus.DoubleCheck,
+                _ => throw new MatchException("The game could not compute the current check state")
+            };
         }
     }
 }
