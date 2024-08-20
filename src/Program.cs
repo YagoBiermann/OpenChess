2b655872e8b25752c0dca200f76adc76682c61ca
YagoBiermann <bsfyagobsf@gmail.com>
2024-08-20 14:36:13 -0300

    build: enforce https redirection

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,29 @@
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
+//Enforce secure connections
+builder.Services.AddHttpsRedirection(options =>
+{
+    if (!builder.Environment.IsDevelopment())
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+    }
+    else
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.TemporaryRedirect;
+    }
+
+    options.HttpsPort = int.Parse(builder.Configuration.GetConnectionString("https_port")!);
+});
+
+
+builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.Run();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
 app.Run();
