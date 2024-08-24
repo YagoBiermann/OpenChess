1d97661d817b118ee347d7da1b35620f40f80221
YagoBiermann <bsfyagobsf@gmail.com>
2024-08-24 00:50:39 -0300

    feat: add command to play moves

---
+++ src/OpenChess.Application/CommandHandlers/PlayMoveHandler.cs
@@ -0,0 +1,36 @@
+using MediatR;
+using OpenChess.Domain;
+
+namespace OpenChess.Application
+{
+    internal class PlayMoveHandler(IMatchRepository matchRepository, MoveTrackingService moveTrackingService) : IRequestHandler<PlayMoveCommand>
+    {
+        private readonly IMatchRepository _matchRepository = matchRepository;
+        private readonly MoveTrackingService _moveTrackingService = moveTrackingService;
+
+        public async Task Handle(PlayMoveCommand request, CancellationToken cancellationToken)
+        {
+            Guid moveId = GenerateMoveId(request);
+            string? storedMoveId = await _moveTrackingService.GetMoveAsync(request.MatchId, moveId.ToString());
+            if (storedMoveId is not null) { return; };
+
+            MatchInfo matchInfo = await _matchRepository.GetById(request.MatchId) ?? throw new MatchException("Match not found!");
+            Match match = new(matchInfo);
+            _ = Guid.TryParse(request.PlayerId, out Guid playerId);
+            Move move = new(playerId, Coordinate.GetInstance(request.Origin), Coordinate.GetInstance(request.Destination), request.Promoting);
+            match.Play(move);
+            await _matchRepository.Update(match.ToInfo());
+            await _moveTrackingService.AddMoveAsync(request.MatchId, moveId.ToString());
+        }
+
+        private static Guid GenerateMoveId(PlayMoveCommand request)
+        {
+            var data = $"{request.MatchId}:{request.PlayerId}:{request.Origin}:{request.Destination}:{request.Promoting ?? string.Empty}";
+            var hashBytes = System.Security.Cryptography.SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(data));
+            var guidBytes = new byte[16];
+            Array.Copy(hashBytes, guidBytes, 16);
+
+            return new Guid(guidBytes);
+        }
+    }
+}
---
+++ src/OpenChess.Application/Commands/PlayMoveCommand.cs
@@ -0,0 +1,6 @@
+using MediatR;
+
+namespace OpenChess.Application
+{
+    public record struct PlayMoveCommand(string MatchId, string PlayerId, string Origin, string Destination, string? Promoting = null) : IRequest;
+}
