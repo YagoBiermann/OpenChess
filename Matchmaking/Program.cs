3454fb7fb09a4b3e5909887ef3574f40a900d6c6
YagoBiermann <bsfyagobsf@gmail.com>
2024-08-24 02:09:09 -0300

    feat: setup httpclient

--- Matchmaking/Program.cs
+++ Matchmaking/Program.cs
@@ -1,7 +1,17 @@
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 var environment = builder.Environment.EnvironmentName;
 builder.Configuration.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
+// http client
+builder.Services.AddHttpClient("HttpClient", client =>
+{
+    var header = builder.Configuration["Headers.ApiKey"] ?? throw new Exception("Header not set!");
+    var value = Environment.GetEnvironmentVariable("INTERNAL_API_KEY") ?? throw new Exception("Internal API Key not set!");
+    client.DefaultRequestHeaders.Add(header, value);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+    client.BaseAddress = new Uri(builder.Configuration["BaseUrl"] ?? throw new Exception("Base Url not set!"));
+});
 builder.Services.AddControllers();
 var app = builder.Build();
 
