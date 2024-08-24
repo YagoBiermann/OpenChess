74c9a119d0193f419363547ee5c6dfe34d56e925
YagoBiermann <bsfyagobsf@gmail.com>
2024-08-24 00:56:24 -0300

    feat: handle user connection to signalr

--- src/OpenChess.Application/Controllers/ChessHub.cs
+++ src/OpenChess.Application/Controllers/ChessHub.cs
@@ -1,3 +1,6 @@
+using System.IdentityModel.Tokens.Jwt;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 namespace OpenChess.Application
 {
@@ -6,6 +9,15 @@ namespace OpenChess.Application
         private readonly IMediator _mediator = mediator;
         private readonly ConnectionTrackingService _connectionTrackingService = connectionTrackingService;
         private readonly MatchTrackingService _matchTrackingService = matchTrackingService;
+
+        [Authorize]
+        public override async Task OnConnectedAsync()
+        {
+            var playerId = (Context.User?.FindFirst(JwtRegisteredClaimNames.Sub)?.Value) ?? throw new HubException("PlayerId not found!");
+            await _connectionTrackingService.AddConnectionAsync(playerId, Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
     }
 
 }
