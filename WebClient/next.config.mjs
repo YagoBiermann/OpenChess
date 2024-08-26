8d8e8a90844887a773bd3b10a96f64ce6188cca5
YagoBiermann <bsfyagobsf@gmail.com>
2024-08-25 22:09:19 -0300

    refactor: add fast refresh for nextjs in docker container

--- WebClient/next.config.mjs
+++ WebClient/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  webpack: (config, context) => {
+    // Enable polling based on env variable being set
+    if (process.env.NEXT_WEBPACK_USEPOLLING) {
+      config.watchOptions = {
+        aggregateTimeout: 200,
+        poll: 1000,
+      };
+    }
+    return config;
+  },
+};
 export default nextConfig;
