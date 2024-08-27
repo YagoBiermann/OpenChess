d64f0f571de49e8d3e7c693ce5d6608c1e5841f8
YagoBiermann <bsfyagobsf@gmail.com>
2024-08-27 16:20:13 -0300

    refactor: return json

--- GameServer/OpenChess.Application/Controllers/MatchController.cs
+++ GameServer/OpenChess.Application/Controllers/MatchController.cs
@@ -188,7 +188,7 @@ namespace OpenChess.Application
             }
             string token = GenerateToken(playerId);
             Response.Headers.Append("Authorization", $"Bearer {token}");
-            return CreatedAtAction(nameof(CreatePlayer), new { playerId }, playerId);
+            return Ok(new { PlayerId = playerId });
         }
 
         private string GenerateToken(string playerId)
