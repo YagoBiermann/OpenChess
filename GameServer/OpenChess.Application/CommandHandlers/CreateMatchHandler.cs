d5debb0bf2e3826118fc23f8e195d2b3a47646e9
YagoBiermann <bsfyagobsf@gmail.com>
2024-09-02 17:04:07 -0300

    refactor: use MatchDTO as the return type

--- GameServer/OpenChess.Application/CommandHandlers/CreateMatchHandler.cs
+++ GameServer/OpenChess.Application/CommandHandlers/CreateMatchHandler.cs
@@ -3,11 +3,11 @@ using OpenChess.Domain;
 
 namespace OpenChess.Application
 {
-    internal class CreateMatchHandler : IRequestHandler<CreateMatchCommand, MatchInfo>
+    internal class CreateMatchHandler : IRequestHandler<CreateMatchCommand, MatchDTO>
     {
         private readonly IMatchRepository _matchRepository;
         public CreateMatchHandler(IMatchRepository matchRepository) { _matchRepository = matchRepository; }
-        public async Task<MatchInfo> Handle(CreateMatchCommand request, CancellationToken cancellationToken)
+        public async Task<MatchDTO> Handle(CreateMatchCommand request, CancellationToken cancellationToken)
         {
             Match match = new(request.Time);
             MatchInfo matchInfo = match.ToInfo();
