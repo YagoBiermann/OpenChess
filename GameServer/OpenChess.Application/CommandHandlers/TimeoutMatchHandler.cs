d9d51da115612363f7942ea0718b26a146cd65c3
YagoBiermann <bsfyagobsf@gmail.com>
2024-09-04 18:53:27 -0300

    refactor: return EndGameDTO

--- GameServer/OpenChess.Application/CommandHandlers/TimeoutMatchHandler.cs
+++ GameServer/OpenChess.Application/CommandHandlers/TimeoutMatchHandler.cs
@@ -3,17 +3,17 @@ using OpenChess.Domain;
 
 namespace OpenChess.Application
 {
-    internal class TimeoutMatchHandler(IMatchRepository matchRepository) : IRequestHandler<TimeoutMatchCommand, MatchDTO>
+    internal class TimeoutMatchHandler(IMatchRepository matchRepository) : IRequestHandler<TimeoutMatchCommand, EndGameDTO>
     {
         private readonly IMatchRepository _matchRepository = matchRepository;
 
-        public async Task<MatchDTO> Handle(TimeoutMatchCommand request, CancellationToken cancellationToken)
+        public async Task<EndGameDTO> Handle(TimeoutMatchCommand request, CancellationToken cancellationToken)
         {
             IMatch match = await _matchRepository.GetById(request.MatchId) ?? throw new MatchException("Match not found!");
             match.FinishWithTimeout(request.PlayerId);
             await _matchRepository.Update(match);
 
-            return MatchDTOMapper.ToDTO(match);
+            return new((char)match.Winner!.Value, match.Status.ToString());
         }
     }
 }
--- GameServer/OpenChess.Application/Commands/TimeoutMatchCommand.cs
+++ GameServer/OpenChess.Application/Commands/TimeoutMatchCommand.cs
@@ -1,7 +1,6 @@
 using MediatR;
-using OpenChess.Domain;
 
 namespace OpenChess.Application
 {
-    public record struct TimeoutMatchCommand(string MatchId, string? PlayerId = null) : IRequest<MatchDTO>;
+    public record struct TimeoutMatchCommand(string MatchId, string? PlayerId = null) : IRequest<EndGameDTO>;
 }
