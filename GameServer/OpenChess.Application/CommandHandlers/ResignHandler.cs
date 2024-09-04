bca8fde4d3e2568bd07b0ec3597f2debb9ba1378
YagoBiermann <bsfyagobsf@gmail.com>
2024-09-04 18:20:04 -0300

    refactor: return ResignDTO from command handler

--- GameServer/OpenChess.Application/CommandHandlers/ResignHandler.cs
+++ GameServer/OpenChess.Application/CommandHandlers/ResignHandler.cs
@@ -3,17 +3,17 @@ using OpenChess.Domain;
 
 namespace OpenChess.Application
 {
-    internal class ResignHandler(IMatchRepository matchRepository) : IRequestHandler<ResignCommand, MatchDTO>
+    internal class ResignHandler(IMatchRepository matchRepository) : IRequestHandler<ResignCommand, ResignDTO>
     {
         private readonly IMatchRepository _matchRepository = matchRepository;
 
-        public async Task<MatchDTO> Handle(ResignCommand request, CancellationToken cancellationToken)
+        public async Task<ResignDTO> Handle(ResignCommand request, CancellationToken cancellationToken)
         {
             IMatch match = await _matchRepository.GetById(request.MatchId) ?? throw new MatchException("Match not found!");
             match.FinishWithResign(request.PlayerId);
             await _matchRepository.Update(match);
 
-            return MatchDTOMapper.ToDTO(match);
+            return new((char)match.Winner!.Value, match.Status.ToString());
         }
     }
 }
--- GameServer/OpenChess.Application/Commands/ResignCommand.cs
+++ GameServer/OpenChess.Application/Commands/ResignCommand.cs
@@ -1,7 +1,6 @@
 using MediatR;
-using OpenChess.Domain;
 
 namespace OpenChess.Application
 {
-    public record struct ResignCommand(string MatchId, string PlayerId) : IRequest<MatchDTO>;
+    public record struct ResignCommand(string MatchId, string PlayerId) : IRequest<ResignDTO>;
 }
