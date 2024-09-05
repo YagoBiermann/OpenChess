ac11ff60c635f44c06f6a3ac1a45713b72d67a54
YagoBiermann <bsfyagobsf@gmail.com>
2024-09-05 14:24:43 -0300

    refactor: remove unnecessary classes

--- GameServer/OpenChess.Application/common/PlayerDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace OpenChess.Application
-{
-    public struct PlayerDTO(string id, char color, string timeRemaining)
-    {
-        public string Id { get; } = id;
-        public char Color { get; } = color;
-        public string TimeRemaining { get; } = timeRemaining;
-    }
-}
