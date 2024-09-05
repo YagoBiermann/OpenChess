6712e2f8ea53f1cf5a6d4fd09ce335b34994bc72
YagoBiermann <bsfyagobsf@gmail.com>
2024-09-05 14:44:53 -0300

    refactor: remove property

--- GameServer/OpenChess.Application/common/JoinMatchDTO.cs
+++ GameServer/OpenChess.Application/common/JoinMatchDTO.cs
@@ -1,8 +1,7 @@
 namespace OpenChess.Application
 {
-    public readonly struct JoinMatchDTO(string matchId, string fen, char color, int time)
+    public readonly struct JoinMatchDTO(string fen, char color, int time)
     {
-        public string MatchId { get; } = matchId;
         public string Fen { get; } = fen;
         public char Color { get; } = color;
         public int Time { get; } = time;
