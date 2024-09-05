5d3d1a7f073813aeab56e8ab3ea1cea89a4047d6
YagoBiermann <bsfyagobsf@gmail.com>
2024-09-05 15:16:27 -0300

    refactor: remove redundant DTO

--- GameServer/OpenChess.Application/common/EndGameDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace OpenChess.Application
-{
-    public readonly struct EndGameDTO(char winner, string status)
-    {
-        public string Status { get; } = status;
-        public char Winner { get; } = winner;
-    }
-}
